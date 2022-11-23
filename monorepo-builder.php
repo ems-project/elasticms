155e2e6aa5fb238e0aa9e3b5c76c29f560751061
mdk <mathieu.dekeyzer@smals.be>
2022-11-23 09:10:34 +0100

    feat: composer files merged

---
+++ monorepo-builder.php
@@ -0,0 +1,38 @@
+<?php
+
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symplify\ComposerJsonManipulator\ValueObject\ComposerJsonSection;
+use Symplify\MonorepoBuilder\ValueObject\Option;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $parameters = $containerConfigurator->parameters();
+
+    $parameters->set(Option::PACKAGE_DIRECTORIES, [
+        __DIR__ . '/EMS',
+        __DIR__ . '/elasticms-cli',
+    ]);
+
+    // how skip packages in loaded direectories?
+//    $parameters->set(Option::PACKAGE_DIRECTORIES_EXCLUDES, [__DIR__ . '/packages/secret-package']);
+
+    // "merge" command related
+
+    // what extra parts to add after merge?
+//    $parameters->set(Option::DATA_TO_APPEND, [
+//        ComposerJsonSection::AUTOLOAD_DEV => [
+//            'psr-4' => [
+//                'Symplify\Tests\\' => 'tests',
+//            ],
+//        ],
+//        ComposerJsonSection::REQUIRE_DEV => [
+//            'phpstan/phpstan' => '^0.12',
+//        ],
+//    ]);
+//
+//    $parameters->set(Option::DATA_TO_REMOVE, [
+//        ComposerJsonSection::REQUIRE => [
+//            // the line is removed by key, so version is irrelevant, thus *
+//            'phpunit/phpunit' => '*',
+//        ],
+//    ]);
+};

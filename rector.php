5667b12d346d5ad69596b8fe05837967c7d155d8
mdk <mathieu.dekeyzer@smals.be>
2022-11-23 16:12:21 +0100

    feat: rector config

---
+++ rector.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
+use Rector\Config\RectorConfig;
+use Rector\Set\ValueObject\LevelSetList;
+use \Rector\Symfony\Set\SymfonySetList;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->paths([
+        __DIR__ . '/EMS',
+        __DIR__ . '/elasticms-cli',
+    ]);
+
+    $rectorConfig->rule(InlineConstructorDefaultToPropertyRector::class);
+
+    $rectorConfig->sets([
+        LevelSetList::UP_TO_PHP_74,
+        SymfonySetList::SYMFONY_54,
+        SymfonySetList::SYMFONY_CODE_QUALITY,
+        SymfonySetList::SYMFONY_CONSTRUCTOR_INJECTION,
+    ]);
+
+    $rectorConfig->importNames();
+    $rectorConfig->importShortClasses(false);
+};

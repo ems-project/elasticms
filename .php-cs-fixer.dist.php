1155960802f2717433c1f81ce869909a70308c0b
mdk <mathieu.dekeyzer@smals.be>
2022-11-23 10:40:17 +0100

    feat:migrate phpcs

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,30 @@
+<?php
+
+if (!file_exists(__DIR__.'/elasticms-cli/src')) {
+    exit(0);
+}
+
+$finder = PhpCsFixer\Finder::create()
+    ->in(__DIR__.'/elasticms-cli/src')
+    ->in(__DIR__.'/elasticms-cli/tests')
+    ->exclude('/elasticms-clitests/tmp')
+    ->in(__DIR__.'/EMS/common-bundle/src')
+    ->in(__DIR__.'/EMS/common-bundle/tests')
+    ->in(__DIR__.'/EMS/helpers/src')
+    ->in(__DIR__.'/EMS/helpers/tests')
+    ->exclude('/EMS/helpers/tmp')
+;
+
+$config = new PhpCsFixer\Config();
+
+return $config
+    ->setRules([
+        '@Symfony' => true,
+        //'declare_strict_types' => true,
+        //'final_class' => true,
+        'native_function_invocation' => ['include' => ['@all']],
+        'no_unused_imports' => true,
+    ])
+    ->setRiskyAllowed(true)
+    ->setFinder($finder)
+;

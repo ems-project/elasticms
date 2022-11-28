d6df58aa063e95d3edbfa56f449ffd0a766b41b5
David mattei <d.mattei89@gmail.com>
2022-11-28 13:55:42 +0100

    fix: notification dashboard (#137)

--- rector.php
+++ rector.php
@@ -5,14 +5,17 @@ declare(strict_types=1);
 use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
 use Rector\Config\RectorConfig;
 use Rector\Set\ValueObject\LevelSetList;
-use \Rector\Symfony\Set\SymfonySetList;
+use Rector\Symfony\Set\SymfonySetList;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->paths([
         __DIR__ . '/EMS',
-        __DIR__ . '/elasticms-admin',
-        __DIR__ . '/elasticms-cli',
-        __DIR__ . '/elasticms-web',
+        __DIR__ . '/elasticms-admin/src',
+        __DIR__ . '/elasticms-cli/src',
+        __DIR__ . '/elasticms-web/src',
+        __DIR__ . '/elasticms-admin/tests',
+        __DIR__ . '/elasticms-cli/tests',
+        __DIR__ . '/elasticms-web/tests',
     ]);
 
     $rectorConfig->rule(InlineConstructorDefaultToPropertyRector::class);

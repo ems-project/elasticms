b4e2e0ddc33f5016dd2066c1f723d69e99a9fccd
David mattei <d.mattei89@gmail.com>
2022-11-28 10:51:06 +0100

    chore: release ci (#135)

--- monorepo-builder.php
+++ monorepo-builder.php
@@ -1,40 +1,15 @@
 <?php
 
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
-use Symplify\ComposerJsonManipulator\ValueObject\ComposerJsonSection;
-use Symplify\MonorepoBuilder\ValueObject\Option;
+declare(strict_types=1);
 
-return static function (ContainerConfigurator $containerConfigurator): void {
-    $parameters = $containerConfigurator->parameters();
+use Symplify\MonorepoBuilder\Config\MBConfig;
 
-    $parameters->set(Option::PACKAGE_DIRECTORIES, [
+return static function (MBConfig $config): void {
+
+    $config->packageDirectories([
         __DIR__ . '/EMS',
         __DIR__ . '/elasticms-cli',
         __DIR__ . '/elasticms-web',
         __DIR__ . '/elasticms-admin',
     ]);
-
-    // how skip packages in loaded direectories?
-//    $parameters->set(Option::PACKAGE_DIRECTORIES_EXCLUDES, [__DIR__ . '/packages/secret-package']);
-
-    // "merge" command related
-
-    // what extra parts to add after merge?
-//    $parameters->set(Option::DATA_TO_APPEND, [
-//        ComposerJsonSection::AUTOLOAD_DEV => [
-//            'psr-4' => [
-//                'Symplify\Tests\\' => 'tests',
-//            ],
-//        ],
-//        ComposerJsonSection::REQUIRE_DEV => [
-//            'phpstan/phpstan' => '^0.12',
-//        ],
-//    ]);
-//
-//    $parameters->set(Option::DATA_TO_REMOVE, [
-//        ComposerJsonSection::REQUIRE => [
-//            // the line is removed by key, so version is irrelevant, thus *
-//            'phpunit/phpunit' => '*',
-//        ],
-//    ]);
 };

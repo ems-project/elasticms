c675c56c62cddae1762e435148997087d0c42dab
David mattei <d.mattei89@gmail.com>
2022-12-09 07:37:51 +0100

    feat(media-library): add new translation domain emsco-component

--- EMS/core-bundle/src/EMSCoreBundle.php
+++ EMS/core-bundle/src/EMSCoreBundle.php
@@ -12,6 +12,7 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 class EMSCoreBundle extends Bundle
 {
     final public const TRANS_DOMAIN = 'EMSCoreBundle';
+    final public const TRANS_COMPONENT = 'emsco-component';
     final public const TRANS_FORM_DOMAIN = 'emsco-forms';
     final public const TRANS_TWIG_DOMAIN = 'emsco-twigs';
     final public const TRANS_DOMAIN_VALIDATORS = 'emsco_validators';

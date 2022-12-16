373eee7df1919a98fe8ac660e0c5519d168294f6
Mathieu De Keyzer <me@theus.be>
2022-12-16 11:18:12 +0100

    fix: no error if invalid JSON, just a JSON formatter

--- EMS/core-bundle/src/Form/DataTransformer/ChannelOptionsTransformer.php
+++ EMS/core-bundle/src/Form/DataTransformer/ChannelOptionsTransformer.php
@@ -37,11 +37,12 @@ final class ChannelOptionsTransformer implements DataTransformerInterface
      */
     private function jsonFormat(array $value, string $attribute): string
     {
-        $defaultFormatted = (isset($value[$attribute]) && '' !== $value[$attribute]) ? $value[$attribute] : '{}';
-        $formatted = \json_decode($defaultFormatted, true, 512, JSON_THROW_ON_ERROR);
+        try {
+            $formatted = \json_decode($value[$attribute] ?? '', true, 512, JSON_THROW_ON_ERROR);
+        } catch (\Throwable $e) {
+            return $value[$attribute] ?? '';
+        }
 
-        return (null !== $formatted && \json_encode($formatted, JSON_PRETTY_PRINT))
-            ? \json_encode($formatted, JSON_PRETTY_PRINT)
-            : '{}';
+        return \json_encode($formatted, JSON_PRETTY_PRINT);
     }
 }

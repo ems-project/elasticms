6f17009de585b7aec5240e0efe560857dc6e3364
David mattei <d.mattei89@gmail.com>
2022-12-29 23:37:15 +0100

    ci(release): improve code and changeLog command (#198)

--- release/Command/GithubReleaseCommand.php
+++ release/Command/GithubReleaseCommand.php
@@ -5,28 +5,33 @@ declare(strict_types=1);
 namespace EMS\Release\Command;
 
 use EMS\Release\Config;
-use Github\AuthMethod;
-use Github\Client as ClientGithub;
-use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Style\SymfonyStyle;
 
-abstract class AbstractGithubRelease extends AbstractGithubCommand
+class GithubReleaseCommand extends AbstractGithubCommand
 {
-    private string $branch;
+    private string $type;
+
+    private string $target;
     private string $version;
-    private string $previousVersion;
+    private ?string $previousVersion = null;
     private bool $force;
+
+    public function __construct(string $type, string $description)
+    {
+        parent::__construct(\sprintf('github:release:%s', $type));
+        $this->setDescription($description);
+        $this->type = $type;
+    }
 
     protected function configure(): void
     {
         $this
-            ->addArgument('branch', InputArgument::REQUIRED, 'branch')
-            ->addArgument('version', InputArgument::REQUIRED, 'version')
-            ->addArgument('previousVersion', InputArgument::REQUIRED, 'previousVersion')
+            ->addArgument('version', InputArgument::REQUIRED, '(new) version')
+            ->addArgument('target', InputArgument::REQUIRED, 'branchName or hash')
+            ->addArgument('previousVersion', InputArgument::OPTIONAL, 'previousVersion')
             ->addOption('force', null, InputOption::VALUE_NONE, 'overwrite release')
         ;
     }
@@ -35,12 +40,22 @@ abstract class AbstractGithubRelease extends AbstractGithubCommand
     {
         parent::initialize($input, $output);
 
-        $this->branch = (string) $input->getArgument('branch');
+        $this->target = (string) $input->getArgument('target');
         $this->version = (string) $input->getArgument('version');
-        $this->previousVersion = (string) $input->getArgument('previousVersion');
+        $this->previousVersion = $input->getArgument('previousVersion');
         $this->force = true === $input->getOption('force');
     }
 
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $this->io->title('GitHub : Release : '.\ucfirst($this->type));
+
+        return $this->release(Config::REPOSITORIES[$this->type]);
+    }
+
+    /**
+     * @param array<string, string> $repositories
+     */
     protected function release(array $repositories): int
     {
         $pg = $this->io->createProgressBar(\count($repositories));
@@ -71,7 +86,7 @@ abstract class AbstractGithubRelease extends AbstractGithubCommand
         $pg->finish();
         $this->io->newLine(2);
 
-        $this->io->table([ 'package', 'status', 'sha', 'url'], $rows);
+        $this->io->table(['package', 'status', 'sha', 'url'], $rows);
 
         return 0;
     }
@@ -82,7 +97,7 @@ abstract class AbstractGithubRelease extends AbstractGithubCommand
 
         $release = $this->githubApi->repo()->releases()->create(self::ORG, $name, [
             'tag_name' => $this->version,
-            'target_commitish' => $this->branch,
+            'target_commitish' => $this->target,
             'name' => $releaseNotes['name'],
             'body' => $releaseNotes['body'],
         ]);
@@ -90,12 +105,15 @@ abstract class AbstractGithubRelease extends AbstractGithubCommand
         return $release['html_url'];
     }
 
-    private function deleteRelease(string $name, int $releaseId)
+    private function deleteRelease(string $name, int $releaseId): void
     {
         $this->githubApi->repo()->releases()->remove(self::ORG, $name, $releaseId);
         $this->githubApi->git()->references()->remove(self::ORG, $name, 'tags/'.$this->version);
     }
 
+    /**
+     * @return ?array<mixed>
+     */
     private function getRelease(string $name): ?array
     {
         try {
@@ -112,12 +130,15 @@ abstract class AbstractGithubRelease extends AbstractGithubCommand
         return $ref['object']['sha'];
     }
 
+    /**
+     * @return array<mixed>
+     */
     private function generateNotes(string $name): array
     {
-        return $this->githubApi->repo()->releases()->generateNotes(self::ORG, $name, [
+        return $this->githubApi->repo()->releases()->generateNotes(self::ORG, $name, \array_filter([
             'tag_name' => $this->version,
-            'target_commitish' => $this->branch,
-            'previous_tag_name' => $this->previousVersion
-        ]);
+            'target_commitish' => $this->target,
+            'previous_tag_name' => $this->previousVersion,
+        ]));
     }
 }

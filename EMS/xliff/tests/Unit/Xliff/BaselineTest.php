ac3a1efc2f0e800a1e692eda43a88d24f04e9ce4
Mathieu De Keyzer <me@theus.be>
2022-12-30 12:47:06 +0100

    test: test inserter with empty elements

--- EMS/xliff/tests/Unit/Xliff/BaselineTest.php
+++ EMS/xliff/tests/Unit/Xliff/BaselineTest.php
@@ -4,11 +4,33 @@ declare(strict_types=1);
 
 namespace EMS\Xliff\Tests\Unit\Xliff;
 
+use EMS\Xliff\Xliff\Entity\InsertReport;
 use EMS\Xliff\Xliff\Extractor;
+use EMS\Xliff\Xliff\Inserter;
 use PHPUnit\Framework\TestCase;
 
 class BaselineTest extends TestCase
 {
+    public function testLoadBaseline2(): void
+    {
+        $inserter = Inserter::fromFile(\join(DIRECTORY_SEPARATOR, [__DIR__, '..', 'Resources', 'Baseline', 'baseline2.xlf']));
+        foreach ($inserter->getDocuments() as $document) {
+            $this->assertEquals('9055abe4a93f3f7e435cc96860116c274fd52f2c', $document->getOuuid());
+            $this->assertEquals('1018373', $document->getRevisionId());
+
+            $insertReport = new InsertReport();
+            $extractedRawData = [];
+            $insertRawData = [];
+            $document->extractTranslations($insertReport, $extractedRawData, $insertRawData);
+            $this->assertEquals('Lohn für Arbeitsanfänger', $insertRawData['title'] ?? null);
+            $this->assertEquals('Lohn für Arbeitsanfänger', $insertRawData['title_short'] ?? null);
+//            \file_put_contents(\join(DIRECTORY_SEPARATOR, [__DIR__, '..', 'Resources', 'Baseline', 'baseline2_description.html']), $insertRawData['description']);
+//            \file_put_contents(\join(DIRECTORY_SEPARATOR, [__DIR__, '..', 'Resources', 'Baseline', 'baseline2_introduction.html']), $insertRawData['introduction']);
+            $this->assertEquals(\file_get_contents(\join(DIRECTORY_SEPARATOR, [__DIR__, '..', 'Resources', 'Baseline', 'baseline2_description.html'])), $insertRawData['description'] ?? null);
+            $this->assertEquals(\file_get_contents(\join(DIRECTORY_SEPARATOR, [__DIR__, '..', 'Resources', 'Baseline', 'baseline2_introduction.html'])), $insertRawData['introduction'] ?? null);
+        }
+    }
+
     public function testBaseline1(): void
     {
         $source = \file_get_contents(\join(DIRECTORY_SEPARATOR, [__DIR__, '..', 'Resources', 'Baseline', 'baseline1_source.html']));

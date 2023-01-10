0e30780e855f2de054c9741875851de6310edd1a
Mathieu De Keyzer <me@theus.be>
2023-01-10 10:12:25 +0100

    fix: dont log password in audit documents (#257)
    Co-authored-by: David mattei <d.mattei89@gmail.com>

--- elasticms-cli/src/Client/Audit/AuditResult.php
+++ elasticms-cli/src/Client/Audit/AuditResult.php
@@ -230,7 +230,7 @@ class AuditResult
         }
 
         return \array_filter(\array_merge($init, [
-            'url' => $this->url->getUrl(),
+            'url' => $this->url->getUrl(null, false, false),
             'referer' => $this->url->getReferer(),
             'referer_label' => $this->url->getRefererLabel(),
             'pa11y' => $this->pa11y,
--- elasticms-cli/src/Client/WebToElasticms/Helper/Url.php
+++ elasticms-cli/src/Client/WebToElasticms/Helper/Url.php
@@ -147,14 +147,14 @@ class Url
         return $this->cleanPath($path);
     }
 
-    public function getUrl(string $path = null, bool $withFragment = false): string
+    public function getUrl(string $path = null, bool $withFragment = false, bool $withPassword = true): string
     {
         if (null !== $path) {
             return (new Url($path, $this->getUrl()))->getUrl(null, $withFragment);
         }
         if (\in_array($this->getScheme(), self::ABSOLUTE_SCHEME)) {
             $url = \sprintf('%s:', $this->scheme);
-        } elseif (null !== $this->user && null !== $this->password) {
+        } elseif (null !== $this->user && null !== $this->password && $withPassword) {
             $url = \sprintf('%s://%s:%s@%s', $this->scheme, $this->user, $this->password, $this->host);
         } else {
             $url = \sprintf('%s://%s', $this->scheme, $this->host);
@@ -237,7 +237,7 @@ class Url
 
     public function getId(): string
     {
-        return \sha1($this->getUrl());
+        return \sha1($this->getUrl(null, false, false));
     }
 
     /**

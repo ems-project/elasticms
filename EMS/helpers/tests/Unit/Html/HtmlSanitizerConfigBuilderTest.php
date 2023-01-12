e29a83aed4ce4934c2d532815069880bff2c1b9e
David mattei <d.mattei89@gmail.com>
2023-01-12 10:11:05 +0100

    fix: add tests for drop/allow attributes

--- EMS/helpers/tests/Unit/Html/HtmlSanitizerConfigBuilderTest.php
+++ EMS/helpers/tests/Unit/Html/HtmlSanitizerConfigBuilderTest.php
@@ -17,6 +17,13 @@ class HtmlSanitizerConfigBuilderTest extends TestCase
         $this->factory(['test' => 'test']);
     }
 
+    public function testAllowAttributes(): void
+    {
+        $this->assertArrayNotHasKey('data-test', $this->factory()->getAllowedElements()['div']);
+        $this->assertArrayHasKey('data-test', $this->factory(['allow_attributes' => ['data-test' => 'div']])->getAllowedElements()['div']);
+        $this->assertArrayHasKey('data-test', $this->factory(['allow_attributes' => ['data-test' => '*']])->getAllowedElements()['a']);
+    }
+
     public function testAllowSafeElements(): void
     {
         $this->assertEmpty($this->factory(['allow_safe_elements' => false])->getAllowedElements());
@@ -51,11 +58,19 @@ class HtmlSanitizerConfigBuilderTest extends TestCase
         $this->assertEquals(['a', 'img', 'span'], \array_keys($blockElements));
     }
 
+    public function testDropAttributes(): void
+    {
+        $this->assertArrayHasKey('title', $this->factory()->getAllowedElements()['a']);
+        $this->assertArrayNotHasKey('title', $this->factory(['drop_attributes' => ['title' => 'a']])->getAllowedElements()['a']);
+        $this->assertArrayNotHasKey('title', $this->factory(['drop_attributes' => ['title' => '*']])->getAllowedElements()['div']);
+    }
+
     public function testDropElements(): void
     {
         $this->assertArrayHasKey('a', $this->factory()->getAllowedElements());
         $this->assertArrayNotHasKey('a', $this->factory(['drop_elements' => ['a']])->getAllowedElements());
     }
+
 
     private function factory(array $settings = []): HtmlSanitizerConfig
     {

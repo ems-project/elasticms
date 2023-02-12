60f16ba8bae27536d2f75ff0e463ed1344fbdff5
Mathieu De Keyzer <me@theus.be>
2023-02-12 20:12:55 +0100

    feat: render form in revision

--- EMS/core-bundle/src/Twig/FormRuntime.php
+++ EMS/core-bundle/src/Twig/FormRuntime.php
@@ -5,21 +5,46 @@ declare(strict_types=1);
 namespace EMS\CoreBundle\Twig;
 
 use EMS\CoreBundle\Core\Form\FormManager;
+use EMS\CoreBundle\Entity\ContentType;
+use EMS\CoreBundle\Entity\DataField;
 use EMS\CoreBundle\Entity\Form;
+use EMS\CoreBundle\Entity\Revision;
+use EMS\CoreBundle\Form\Form\RevisionType;
+use EMS\CoreBundle\Service\DataService;
+use Symfony\Component\Form\FormFactory;
+use Symfony\Component\Form\FormInterface;
 
 class FormRuntime
 {
-    public function __construct(protected FormManager $formManager)
+    public function __construct(protected FormManager $formManager, protected FormFactory $formFactory, protected DataService $dataService)
     {
     }
 
-    public function getFormByName(string $name): ?Form
+    /**
+     * @param mixed[] $rawData
+     */
+    public function getFormByName(string $name, array $rawData = []): ?FormInterface
     {
-        $form = $this->formManager->getByItemName($name);
-        if (null !== $form && !$form instanceof Form) {
-            throw new \RuntimeException('Unexpected non-Form entity');
+        $formEntity = $this->formManager->getByItemName($name);
+        if (null === $formEntity) {
+            return null;
         }
 
-        return $form;
+        if (!$formEntity instanceof Form) {
+            throw new \RuntimeException('Unexpected non-Form entity');
+        }
+        $fakeContentType = new ContentType();
+        $fakeContentType->setFieldType($formEntity->getFieldType());
+        $fakeRevision = new Revision();
+        $fakeRevision->setContentType($fakeContentType);
+        $fakeRevision->setRawData($rawData);
+        $form = $this->formFactory->create(RevisionType::class, $fakeRevision, ['raw_data' => $rawData]);
+
+        return $form->get('data');
+    }
+
+    public function getDataField(FormInterface $form): DataField
+    {
+        return $this->dataService->getDataFieldsStructure($form);
     }
 }

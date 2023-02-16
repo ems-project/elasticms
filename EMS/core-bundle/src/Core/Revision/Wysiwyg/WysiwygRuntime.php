67a055b33c1ac5384a599913cf0c1ed1b08fbf29
David mattei <d.mattei89@gmail.com>
2023-02-16 11:48:21 +0100

    feat(wysiwyg): config emsBrowsers

--- EMS/core-bundle/src/Core/Revision/Wysiwyg/WysiwygRuntime.php
+++ EMS/core-bundle/src/Core/Revision/Wysiwyg/WysiwygRuntime.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace EMS\CoreBundle\Core\Revision\Wysiwyg;
 
+use EMS\CoreBundle\Core\Dashboard\DashboardManager;
 use EMS\CoreBundle\Core\User\UserManager;
+use EMS\CoreBundle\Entity\Dashboard;
 use EMS\CoreBundle\Service\WysiwygStylesSetService;
 use EMS\Helpers\Standard\Json;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
@@ -15,7 +17,8 @@ final class WysiwygRuntime implements RuntimeExtensionInterface
     public function __construct(
         private readonly WysiwygStylesSetService $wysiwygStylesSetService,
         private readonly UserManager $userManager,
-        private readonly UrlGeneratorInterface $urlGenerator
+        private readonly UrlGeneratorInterface $urlGenerator,
+        private readonly DashboardManager $dashboardManager
     ) {
     }
 
@@ -50,15 +53,25 @@ final class WysiwygRuntime implements RuntimeExtensionInterface
     }
 
     /**
-     * @return array<string, string>
+     * @return array<string, mixed>
      */
     private function getDefaultConfig(): array
     {
-        return [
+        $config = [
             'imageUploadUrl' => $this->urlGenerator->generate('ems_image_upload_url'),
             'imageBrowser_listUrl' => $this->urlGenerator->generate('ems_images_index'),
             'ems_filesUrl' => $this->urlGenerator->generate('ems_core_uploaded_file_wysiwyg_index'),
         ];
+
+        foreach (Dashboard::DASHBOARD_BROWSERS as $definition) {
+            if ($dashboard = $this->dashboardManager->getDefinition($definition)) {
+                $config['emsBrowsers'][$definition] = $this->urlGenerator->generate('emsco_dashboard_browse', [
+                    'dashboardName' => $dashboard->getName(),
+                ]);
+            }
+        }
+
+        return $config;
     }
 
     /**

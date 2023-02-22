bda546ebe8109fc2dbdef875d2ed0534c050f2f0
Mathieu De Keyzer <me@theus.be>
2023-02-22 14:26:45 +0100

    feat: HTTP headers class

---
+++ EMS/helpers/src/Html/Headers.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EMS\Helpers\Html;
+
+class Headers
+{
+    public const AUTHORIZATION = 'Authorization';
+    public const CONTENT_DISPOSITION = 'Content-Disposition';
+    public const CONTENT_TYPE = 'Content-Type';
+    public const X_ROBOTS_TAG = 'X-Robots-Tag';
+    public const X_ROBOTS_TAG_NOINDEX = 'noindex';
+    public const WWW_AUTHENTICATE = 'WWW-Authenticate';
+}

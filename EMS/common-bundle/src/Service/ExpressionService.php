dbeba7cde65d8af8523d6bbd0ffc768e66b5abdc
David mattei <d.mattei89@gmail.com>
2023-03-24 15:27:37 +0100

    feat(emsco/display): expressionService evaluateToString method

--- EMS/common-bundle/src/Service/ExpressionService.php
+++ EMS/common-bundle/src/Service/ExpressionService.php
@@ -21,22 +21,37 @@ final class ExpressionService implements ExpressionServiceInterface
      */
     public function evaluateToBool(string $expression, array $values = []): bool
     {
+        $evaluate = $this->evaluate($expression, $values);
+
+        return \is_bool($evaluate) ? $evaluate : false;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function evaluateToString(string $expression, array $values = []): ?string
+    {
+        $evaluate = $this->evaluate($expression, $values);
+
+        return \is_string($evaluate) ? $evaluate : null;
+    }
+
+    /**
+     * @param array<mixed> $values
+     */
+    private function evaluate(string $expression, array $values = []): bool|string|null
+    {
         try {
-            $evaluation = $this->getExpressionLanguage()->evaluate($expression, $values);
-
-            if (!\is_bool($evaluation)) {
-                throw new \Exception('Expression did not evaluate to bool!');
-            }
-
-            return $evaluation;
-        } catch (\Exception $e) {
+            return $this->getExpressionLanguage()->evaluate($expression, $values);
+        } catch (\Throwable $e) {
             $this->logger->error('Expression failed: {message}', [
                 'message' => $e->getMessage(),
                 'values' => $values,
                 'expression' => $expression,
+                'noFlash' => true,
             ]);
 
-            return false;
+            return null;
         }
     }
 

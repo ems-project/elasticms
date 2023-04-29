1aba3c3eb30963b760f6335c300e5cf54152367a
Mathieu De Keyzer <me@theus.be>
2023-04-29 10:25:04 +0200

    feat: test not saving in safe method

--- EMS/common-bundle/src/Twig/StoreRuntime.php
+++ EMS/common-bundle/src/Twig/StoreRuntime.php
@@ -5,16 +5,22 @@ declare(strict_types=1);
 namespace EMS\CommonBundle\Twig;
 
 use EMS\CommonBundle\Common\Store\StoreDataHelper;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Twig\Extension\RuntimeExtensionInterface;
 
 final class StoreRuntime implements RuntimeExtensionInterface
 {
-    public function __construct()
-    {
+    public function __construct(
+        private readonly RequestStack $requestStack
+    ) {
     }
 
     public function save(StoreDataHelper $data): void
     {
+        $request = $this->requestStack->getCurrentRequest();
+        if (null !== $request && $request->isMethodSafe()) {
+            throw new \RuntimeException(\sprintf('The safe method %s is not allowed when saving data', $request->getMethod()));
+        }
     }
 
     public function read(string $key): StoreDataHelper

52c163a4b1680ab48282baa374775c6e913dddbc
Mathieu De Keyzer <me@theus.be>
2023-04-29 14:02:19 +0200

    feat: StoreDataFactoryInterface

---
+++ EMS/common-bundle/src/Common/StoreData/Factory/StoreDataFactoryInterface.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace EMS\CommonBundle\Common\StoreData\Factory;
+
+interface StoreDataFactoryInterface
+{
+    public function getType(): string;
+}
--- EMS/common-bundle/src/Common/StoreData/StoreDataManager.php
+++ EMS/common-bundle/src/Common/StoreData/StoreDataManager.php
@@ -4,14 +4,51 @@ declare(strict_types=1);
 
 namespace EMS\CommonBundle\Common\StoreData;
 
+use EMS\CommonBundle\Common\StoreData\Factory\StoreDataFactoryInterface;
+use Psr\Log\LoggerInterface;
+
 class StoreDataManager
 {
+    /** @var StoreDataFactoryInterface[] */
+    private array $factories = [];
+
+    /**
+     * @param iterable<StoreDataFactoryInterface> $factories
+     * @param array<array{type: string}>          $storeDataConfigs
+     */
+    public function __construct(private readonly LoggerInterface $logger, iterable $factories, private readonly array $storeDataConfigs = [])
+    {
+        foreach ($factories as $factory) {
+            if (!$factory instanceof StoreDataFactoryInterface) {
+                throw new \RuntimeException('Unexpected StoreDataFactoryInterface class');
+            }
+            $this->addStoreDataFactory($factory);
+        }
+        $this->registerServicesFromConfigs();
+    }
+
     public function save(StoreDataHelper $data): void
     {
     }
 
     public function read(string $key): StoreDataHelper
     {
-        return new StoreDataHelper();
+        return new StoreDataHelper($key);
+    }
+
+    private function addStoreDataFactory(StoreDataFactoryInterface $factory): void
+    {
+        $this->factories[$factory->getType()] = $factory;
+    }
+
+    private function registerServicesFromConfigs(): void
+    {
+        foreach ($this->storeDataConfigs as $storeDataConfig) {
+            if (!isset($this->factories[$storeDataConfig['type']])) {
+                $this->logger->warning(\sprintf('Store data factory %s not registered', $storeDataConfig['type']));
+                continue;
+            }
+            \dump($storeDataConfig);
+        }
     }
 }

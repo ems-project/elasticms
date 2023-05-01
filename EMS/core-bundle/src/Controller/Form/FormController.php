b77ced5574ce7592ee440612723ab68cc68b6b52
Mathieu De Keyzer <me@theus.be>
2023-05-01 08:37:46 +0200

    feat: form reorder button

--- EMS/core-bundle/src/Controller/Form/FormController.php
+++ EMS/core-bundle/src/Controller/Form/FormController.php
@@ -145,6 +145,7 @@ class FormController extends AbstractController
         $table->addColumn('form.index.column.name', 'name');
         $table->addColumn('form.index.column.label', 'label');
         $table->addItemGetAction(Routes::FORM_ADMIN_EDIT, 'form.actions.edit', 'pencil');
+        $table->addItemGetAction(Routes::FORM_ADMIN_REORDER, 'form.actions.reorder', 'reorder');
         $table->addItemPostAction(Routes::FORM_ADMIN_DELETE, 'form.actions.delete', 'trash', 'form.actions.delete_confirm')->setButtonType('outline-danger');
         $table->addTableAction(TableAbstract::DELETE_ACTION, 'fa fa-trash', 'form.actions.delete_selected', 'form.actions.delete_selected_confirm')
             ->setCssClass('btn btn-outline-danger');

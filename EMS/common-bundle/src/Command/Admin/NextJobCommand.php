def15e6d4174b20a64f13c25eaa191abe22a7b15
Mathieu De Keyzer <me@theus.be>
2023-05-21 13:04:04 +0200

    feat: silent mode

--- EMS/common-bundle/src/Command/Admin/NextJobCommand.php
+++ EMS/common-bundle/src/Command/Admin/NextJobCommand.php
@@ -10,14 +10,17 @@ use EMS\CommonBundle\Common\Command\AbstractCommand;
 use EMS\CommonBundle\Common\Job\JobManager;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Logger\ConsoleLogger;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class NextJobCommand extends AbstractCommand
 {
     public const TAG_ARGUMENT = 'tag';
+    public const SILENT_OPTION = 'silent';
     protected static $defaultName = Commands::ADMIN_NEXT_JOB;
     private string $tag;
+    private bool $silent;
 
     public function __construct(private readonly AdminHelper $adminHelper, private readonly JobManager $jobManager)
     {
@@ -28,6 +31,7 @@ class NextJobCommand extends AbstractCommand
     {
         parent::configure();
         $this->addArgument(self::TAG_ARGUMENT, InputArgument::REQUIRED, 'Tag that identifies the command family');
+        $this->addOption(self::SILENT_OPTION, null, InputOption::VALUE_NONE, 'Dont echo outputs in the console');
     }
 
     public function initialize(InputInterface $input, OutputInterface $output): void
@@ -35,6 +39,7 @@ class NextJobCommand extends AbstractCommand
         parent::initialize($input, $output);
         $this->adminHelper->setLogger(new ConsoleLogger($output));
         $this->tag = $this->getArgumentString(self::TAG_ARGUMENT);
+        $this->silent = $this->getOptionBool(self::SILENT_OPTION);
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
@@ -46,9 +51,11 @@ class NextJobCommand extends AbstractCommand
             return self::EXECUTE_SUCCESS;
         }
 
-        $this->io->title(\sprintf('Starting job %s', $job->getJobId()));
-        $this->io->title(\sprintf('Command: %s', $job->getJobId()));
-        $this->jobManager->run($job);
+        if (!$this->silent) {
+            $this->io->title(\sprintf('Starting job %s', $job->getJobId()));
+            $this->io->write(\sprintf('Command: %s', $job->getCommand()));
+        }
+        $this->jobManager->run($job, $this->silent ? null : $output);
 
         return self::EXECUTE_SUCCESS;
     }

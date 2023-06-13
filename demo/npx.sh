2a26109ccb45f6a8ce7d76fc2b7011694ffbc9e7
Mathieu De Keyzer <me@theus.be>
2023-06-13 17:05:30 +0200

    ref: elasticms/base-php:8.1-cli-dev

--- demo/npx.sh
+++ demo/npx.sh
@@ -1,3 +1,3 @@
 #/bin/bash
 
-docker run -u ${DOCKER_USER:-1001} --rm -it -v $PWD:/opt/src --workdir /opt/src elasticms/base-php:8.1-fpm-dev npx $@
+docker run -u ${DOCKER_USER:-1001} --rm -it -v $PWD:/opt/src --workdir /opt/src elasticms/base-php:8.1-cli-dev npx $@

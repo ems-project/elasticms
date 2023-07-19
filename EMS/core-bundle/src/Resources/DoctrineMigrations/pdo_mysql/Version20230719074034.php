7d3a970ce381191fcaed002876ecda672138e62a
David mattei <d.mattei89@gmail.com>
2023-07-19 09:42:49 +0200

    feat(core/job): add tag field

---
+++ EMS/core-bundle/src/Resources/DoctrineMigrations/pdo_mysql/Version20230719074034.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Platforms\MySQLPlatform;
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20230719074034 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Add a nullable tag field to the job entity';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->abortIf(
+            !$this->connection->getDatabasePlatform() instanceof MySQLPlatform,
+            "Migration can only be executed safely on '\Doctrine\DBAL\Platforms\MySQLPlatform'."
+        );
+
+        $this->addSql('ALTER TABLE job ADD tag VARCHAR(255) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->abortIf(
+            !$this->connection->getDatabasePlatform() instanceof MySQLPlatform,
+            "Migration can only be executed safely on '\Doctrine\DBAL\Platforms\MySQLPlatform'."
+        );
+
+        $this->addSql('ALTER TABLE job DROP tag');
+    }
+}
---
+++ EMS/core-bundle/src/Resources/DoctrineMigrations/pdo_pgsql/Version20230719073401.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Platforms\PostgreSQLPlatform;
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20230719073401 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Add a nullable tag field to the job entity';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->abortIf(
+            !$this->connection->getDatabasePlatform() instanceof PostgreSQLPlatform,
+            "Migration can only be executed safely on '\Doctrine\DBAL\Platforms\PostgreSQLPlatform'."
+        );
+
+        $this->addSql('ALTER TABLE job ADD tag VARCHAR(255) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->abortIf(
+            !$this->connection->getDatabasePlatform() instanceof PostgreSQLPlatform,
+            "Migration can only be executed safely on '\Doctrine\DBAL\Platforms\PostgreSQLPlatform'."
+        );
+
+        $this->addSql('ALTER TABLE job DROP tag');
+    }
+}

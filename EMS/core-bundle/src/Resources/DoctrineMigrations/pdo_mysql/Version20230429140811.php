0413e44f917f00613320e317347e9a47e9185f0e
David mattei <d.mattei89@gmail.com>
2023-07-19 09:44:11 +0200

    chore: add missing platform checks for previous doctrine migrations

--- EMS/core-bundle/src/Resources/DoctrineMigrations/pdo_mysql/Version20230429140811.php
+++ EMS/core-bundle/src/Resources/DoctrineMigrations/pdo_mysql/Version20230429140811.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Application\Migrations;
 
+use Doctrine\DBAL\Platforms\MySQLPlatform;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
@@ -16,11 +17,21 @@ final class Version20230429140811 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
+        $this->abortIf(
+            !$this->connection->getDatabasePlatform() instanceof MySQLPlatform,
+            "Migration can only be executed safely on '\Doctrine\DBAL\Platforms\MySQLPlatform'."
+        );
+
         $this->addSql('CREATE TABLE store_data (id CHAR(36) NOT NULL COMMENT \'(DC2Type:uuid)\', `key` VARCHAR(2048) NOT NULL, data LONGTEXT DEFAULT NULL COMMENT \'(DC2Type:json)\', created DATETIME NOT NULL, modified DATETIME NOT NULL, UNIQUE INDEX UNIQ_4F4A5DAD8A90ABA9 (`key`), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema): void
     {
+        $this->abortIf(
+            !$this->connection->getDatabasePlatform() instanceof MySQLPlatform,
+            "Migration can only be executed safely on '\Doctrine\DBAL\Platforms\MySQLPlatform'."
+        );
+
         $this->addSql('DROP TABLE store_data');
     }
 }

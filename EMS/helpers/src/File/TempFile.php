944b597316cd3eb83f41277ed802e509257e2f9c
Mathieu De Keyzer <me@theus.be>
2023-07-27 12:17:19 +0200

    fix(common): avoid loading file in memory (use TempFile) (#560)
    Co-authored-by: David mattei <d.mattei89@gmail.com>

--- EMS/helpers/src/File/TempFile.php
+++ EMS/helpers/src/File/TempFile.php
@@ -14,18 +14,18 @@ class TempFile
     {
     }
 
-    private static function create(): self
+    public static function create(?string $cacheFolder = null): self
     {
-        if (!$path = \tempnam(\sys_get_temp_dir(), self::PREFIX)) {
+        if (!$path = \tempnam($cacheFolder ?? \sys_get_temp_dir(), self::PREFIX)) {
             throw new \RuntimeException(\sprintf('Could not create temp file in "%s"', \sys_get_temp_dir()));
         }
 
         return new self($path);
     }
 
-    private static function createNamed(string $name): self
+    public static function createNamed(string $name, ?string $cacheFolder = null): self
     {
-        return new self(\implode(\DIRECTORY_SEPARATOR, [\sys_get_temp_dir(), self::PREFIX.$name]));
+        return new self(\implode(\DIRECTORY_SEPARATOR, [$cacheFolder ?? \sys_get_temp_dir(), self::PREFIX.$name]));
     }
 
     public function exists(): bool
@@ -33,28 +33,41 @@ class TempFile
         return \file_exists($this->path);
     }
 
-    public static function fromStream(StreamInterface $stream, ?string $name = null): self
+    public static function fromStream(StreamInterface $stream, ?string $name = null, ?string $cacheFolder = null): self
     {
-        $tempFile = $name ? self::createNamed($name) : self::create();
-
-        if ($tempFile->exists()) {
-            return $tempFile;
+        $tempFile = $name ? self::createNamed($name) : self::create($cacheFolder);
+        if (!$tempFile->exists()) {
+            $tempFile->loadFromStream($stream);
         }
 
-        if (!$handle = \fopen($tempFile->path, 'w')) {
-            throw new \RuntimeException(\sprintf('Can\'t open a temporary file %s', $tempFile->path));
+        return $tempFile;
+    }
+
+    public function loadFromStream(StreamInterface $stream): void
+    {
+        if (!$handle = \fopen($this->path, 'w')) {
+            throw new \RuntimeException(\sprintf('Can\'t open a temporary file %s', $this->path));
         }
 
         while (!$stream->eof()) {
             if (false === \fwrite($handle, $stream->read(8192))) {
-                throw new \RuntimeException(\sprintf('Can\'t write in temporary file %s', $tempFile->path));
+                throw new \RuntimeException(\sprintf('Can\'t write in temporary file %s', $this->path));
             }
         }
 
         if (false === \fclose($handle)) {
-            throw new \RuntimeException(\sprintf('Can\'t close the temporary file %s', $tempFile->path));
+            throw new \RuntimeException(\sprintf('Can\'t close the temporary file %s', $this->path));
         }
+    }
 
-        return $tempFile;
+    public function clean(): void
+    {
+        if (!$this->exists()) {
+            return;
+        }
+        try {
+            @\unlink($this->path);
+        } catch (\Throwable) {
+        }
     }
 }

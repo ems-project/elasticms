38db6ea2db4193135b480c0978106bdbd0e0b450
Mathieu De Keyzer <me@theus.be>
2023-10-10 20:39:06 +0200

    chore: phpcs

--- EMS/core-bundle/src/Controller/ContentManagement/FileController.php
+++ EMS/core-bundle/src/Controller/ContentManagement/FileController.php
@@ -4,7 +4,6 @@ namespace EMS\CoreBundle\Controller\ContentManagement;
 
 use EMS\CommonBundle\Helper\EmsFields;
 use EMS\CommonBundle\Storage\NotFoundException;
-use EMS\CommonBundle\Storage\Processor\Color;
 use EMS\CoreBundle\Entity\UserInterface;
 use EMS\CoreBundle\Service\AssetExtractorService;
 use EMS\CoreBundle\Service\FileService;
@@ -259,7 +258,7 @@ class FileController extends AbstractController
 
     public function browserConfig(): Response
     {
-        $response = $this->render("@EMSCore/core/browserconfig.xml", [
+        $response = $this->render('@EMSCore/core/browserconfig.xml', [
             'themeColor' => $this->themeColor,
         ]);
         $response->setCache([

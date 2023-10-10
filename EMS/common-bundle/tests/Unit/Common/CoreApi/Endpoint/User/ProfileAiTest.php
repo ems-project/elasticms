f5c7ad29c20769caf6cefe5d0521297aae28aa4b
Jerome Coppee <j.coppee@gmail.com>
2023-10-10 12:57:56 +0200

    test(ai/common): add CoreApi/Endpoint/User

---
+++ EMS/common-bundle/tests/Unit/Common/CoreApi/Endpoint/User/ProfileAiTest.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EMS\CommonBundle\Tests\Unit\Common\CoreApi\Endpoint\User;
+
+use EMS\CommonBundle\Common\CoreApi\Endpoint\User\Profile;
+use PHPUnit\Framework\TestCase;
+
+final class ProfileAiTest extends TestCase
+{
+    private Profile $profile;
+
+    protected function setUp(): void
+    {
+        $data = [
+            'id' => 1,
+            'username' => 'testUser',
+            'email' => 'test@example.com',
+            'displayName' => 'Test User',
+            'roles' => ['ROLE_USER', 'ROLE_ADMIN'],
+            'circles' => ['circle1', 'circle2'],
+            'lastLogin' => '2023-10-06T15:00:00+00:00'
+        ];
+        $this->profile = new Profile($data);
+    }
+
+    public function testGetId(): void
+    {
+        $this->assertSame(1, $this->profile->getId());
+    }
+
+    public function testGetUsername(): void
+    {
+        $this->assertSame('testUser', $this->profile->getUsername());
+    }
+
+    public function testGetEmail(): void
+    {
+        $this->assertSame('test@example.com', $this->profile->getEmail());
+    }
+
+    public function testGetDisplayName(): void
+    {
+        $this->assertSame('Test User', $this->profile->getDisplayName());
+    }
+
+    public function testGetRoles(): void
+    {
+        $this->assertSame(['ROLE_USER', 'ROLE_ADMIN'], $this->profile->getRoles());
+    }
+
+    public function testGetCircles(): void
+    {
+        $this->assertSame(['circle1', 'circle2'], $this->profile->getCircles());
+    }
+
+    public function testGetLastLogin(): void
+    {
+        $expectedDate = \DateTimeImmutable::createFromFormat(\DateTimeImmutable::ATOM, '2023-10-06T15:00:00+00:00');
+        $this->assertEquals($expectedDate, $this->profile->getLastLogin());
+    }
+}

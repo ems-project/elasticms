e3eb7607083a7cc2630bf53366057c8920484753
Mathieu De Keyzer <me@theus.be>
2023-10-16 16:53:11 +0200

    ref: AdminHelper is using TokenStore

--- EMS/common-bundle/src/Common/Admin/AdminHelper.php
+++ EMS/common-bundle/src/Common/Admin/AdminHelper.php
@@ -2,11 +2,9 @@
 
 namespace EMS\CommonBundle\Common\Admin;
 
-use EMS\CommonBundle\Common\Standard\Hash;
+use EMS\CommonBundle\Common\CoreApi\TokenStore;
 use EMS\CommonBundle\Contracts\CoreApi\CoreApiFactoryInterface;
 use EMS\CommonBundle\Contracts\CoreApi\CoreApiInterface;
-use Psr\Cache\CacheItemInterface;
-use Psr\Cache\CacheItemPoolInterface;
 use Psr\Log\LoggerInterface;
 
 class AdminHelper
@@ -15,10 +13,8 @@ class AdminHelper
 
     public function __construct(
         private readonly CoreApiFactoryInterface $coreApiFactory,
-        private readonly CacheItemPoolInterface $cache,
+        private readonly TokenStore $tokenStore,
         private LoggerInterface $logger,
-        private readonly ?string $defaultBaseUrl,
-        private readonly ?string $defaultApiToken,
     ) {
     }
 
@@ -32,8 +28,7 @@ class AdminHelper
         $this->coreApi = $this->coreApiFactory->create($baseUrl);
         $this->coreApi->setLogger($this->logger);
         $this->coreApi->authenticate($username, $password);
-        $this->cache->save($this->apiCacheBaseUrl()->set($this->coreApi->getBaseUrl()));
-        $this->cache->save($this->apiCacheToken($this->coreApi)->set($this->coreApi->getToken()));
+        $this->tokenStore->saveToken($this->coreApi->getBaseUrl(), $this->coreApi->getToken());
 
         return $this->coreApi;
     }
@@ -45,7 +40,7 @@ class AdminHelper
         }
         $coreApi = $this->coreApiFactory->create($baseUrl);
         $coreApi->setLogger($this->logger);
-        $token = $this->apiCacheToken($coreApi)->get();
+        $token = $this->tokenStore->getToken($baseUrl);
         if (!\is_string($token)) {
             return false;
         }
@@ -60,36 +55,14 @@ class AdminHelper
         return false;
     }
 
-    private function apiCacheBaseUrl(): CacheItemInterface
-    {
-        return $this->cache->getItem('ems_admin_base_url');
-    }
-
-    private function apiCacheToken(CoreApiInterface $coreApi): CacheItemInterface
-    {
-        return $this->cache->getItem(Hash::string($coreApi->getBaseUrl(), 'token_'));
-    }
-
     public function getCoreApi(): CoreApiInterface
     {
         if (null !== $this->coreApi) {
             return $this->coreApi;
         }
-        $baseUrl = $this->apiCacheBaseUrl()->get();
-        if (!\is_string($baseUrl)) {
-            $baseUrl = $this->defaultBaseUrl;
-        }
-        if (!\is_string($baseUrl)) {
-            throw new \RuntimeException('Not authenticated, run ems:admin:login');
-        }
-        $this->coreApi = $this->coreApiFactory->create($baseUrl);
+        $this->coreApi = $this->coreApiFactory->create($this->tokenStore->giveBaseUrl());
         $this->coreApi->setLogger($this->logger);
-        $apiToken = $this->apiCacheToken($this->coreApi)->get();
-        if (\is_string($apiToken)) {
-            $this->coreApi->setToken($apiToken);
-        } elseif (null !== $this->defaultApiToken) {
-            $this->coreApi->setToken($this->defaultApiToken);
-        }
+        $this->coreApi->setToken($this->tokenStore->giveToken());
 
         return $this->coreApi;
     }

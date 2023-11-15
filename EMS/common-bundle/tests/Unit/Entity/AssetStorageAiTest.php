63edcf0dfa53cbd3930c6fca1236f96bcc32cc4e
Jerome Coppee <j.coppee@gmail.com>
2023-11-15 10:10:18 +0100

    test(common): add Entity/*

---
+++ EMS/common-bundle/tests/Unit/Entity/AssetStorageAiTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace EMS\CommonBundle\Tests\Entity;
+
+use EMS\CommonBundle\Entity\AssetStorage;
+use PHPUnit\Framework\TestCase;
+
+final class AssetStorageAiTest extends TestCase
+{
+    public function testAssetStorageEntity(): void
+    {
+        $asset = new AssetStorage();
+
+        $asset->setHash('test_hash');
+        $asset->setContents('test_contents');
+        $asset->setSize(123);
+        $asset->setConfirmed(true);
+
+        $this->assertEquals('test_hash', $asset->getHash());
+        $this->assertEquals('test_contents', $asset->getContents());
+        $this->assertEquals(123, $asset->getSize());
+        $this->assertTrue($asset->isConfirmed());
+    }
+
+    public function testUpdateModified(): void
+    {
+        $asset = new AssetStorage();
+
+        $asset->updateModified();
+
+        $this->assertInstanceOf(\DateTime::class, $asset->getModified());
+        $this->assertInstanceOf(\DateTime::class, $asset->getCreated());
+    }
+
+    public function testUnexpectedNullValues(): void
+    {
+        $asset = new AssetStorage();
+
+        $this->expectException(\RuntimeException::class);
+        $asset->getHash();
+
+        $this->expectException(\RuntimeException::class);
+        $asset->getModified();
+
+        $this->expectException(\RuntimeException::class);
+        $asset->getCreated();
+
+        $this->expectException(\RuntimeException::class);
+        $asset->getSize();
+
+        $this->expectException(\RuntimeException::class);
+        $asset->isConfirmed();
+    }
+}
---
+++ EMS/common-bundle/tests/Unit/Entity/CreatedModifiedTraitAiTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace EMS\CommonBundle\Tests\Entity;
+
+use EMS\CommonBundle\Entity\CreatedModifiedTrait;
+use PHPUnit\Framework\TestCase;
+
+class DummyEntity
+{
+    use CreatedModifiedTrait;
+
+    public function __construct()
+    {
+        $this->created = new \DateTime();
+    }
+}
+
+final class CreatedModifiedTraitAiTest extends TestCase
+{
+    public function testTimestamps(): void
+    {
+        $entity = new DummyEntity();
+
+        // Test that the created timestamp is set on creation
+        $this->assertInstanceOf(\DateTimeInterface::class, $entity->getCreated());
+
+        // Sleep for a second to ensure the modified timestamp will be different
+        sleep(1);
+
+        // Test that the modified timestamp is updated
+        $entity->updateModified();
+        $this->assertInstanceOf(\DateTimeInterface::class, $entity->getModified());
+        $this->assertGreaterThan($entity->getCreated(), $entity->getModified());
+
+        // Test setting custom timestamps
+        $past = new \DateTime('-1 day');
+        $entity->setCreated($past);
+        $entity->setModified($past);
+
+        $this->assertEquals($past, $entity->getCreated());
+        $this->assertEquals($past, $entity->getModified());
+    }
+}

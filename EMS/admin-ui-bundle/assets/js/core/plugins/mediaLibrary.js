9c45c274dc64c7c2335bb03b270174f6b1d420a9
Mathieu De Keyzer <me@theus.be>
2024-01-10 15:23:51 +0100

    fix: no this in anonymous function

--- EMS/admin-ui-bundle/assets/js/core/plugins/mediaLibrary.js
+++ EMS/admin-ui-bundle/assets/js/core/plugins/mediaLibrary.js
@@ -7,10 +7,11 @@ class MediaLibrary {
 
   load (target) {
     const elements = target.getElementsByClassName('media-lib')
-    const bodyData = target.querySelector('body').dataset;
+    const bodyData = target.querySelector('body').dataset
+    const self = this;
 
     [].forEach.call(elements, function (el) {
-      this.components.push(new Component(el, {
+      self.components.push(new Component(el, {
         urlMediaLib: '/component/media-lib',
         urlInitUpload: bodyData.initUpload,
         hashAlgo: bodyData.hashAlgo

030b980ec767abb93a51307aaabadb1209f06712
Mathieu De Keyzer <me@theus.be>
2024-02-03 15:49:53 +0100

    ref: Ajax save form

--- EMS/admin-ui-bundle/_to_be_ref/js/initEms.js
+++ EMS/admin-ui-bundle/_to_be_ref/js/initEms.js
@@ -95,54 +95,6 @@ import ajaxModal from "../../js/core/helpers/ajaxModal";
         window.jsonMenuNested = jsonMenuNestedList;
     }
 
-    function initAjaxFormSave() {
-        $('button[data-ajax-save-url]').each(function(){
-            const button = $(this);
-            const form = button.closest('form');
-
-            const ajaxSave = function(event){
-                event.preventDefault();
-
-                const formContent = form.serialize();
-                window.ajaxRequest.post(button.data('ajax-save-url'), formContent)
-                    .success(function(message) {
-                        let response = message;
-                        if ( ! response instanceof Object ) {
-                            response = jQuery.parseJSON( message );
-                        }
-
-                        $('.has-error').removeClass('has-error');
-
-                        $(response.errors).each(function(index, item){
-                            $('#'+item.propertyPath).parent().addClass('has-error');
-                        });
-                    });
-            };
-
-            button.on('click', ajaxSave);
-
-            $(document).keydown(function(e) {
-                let key = undefined;
-                const possible = [ e.key, e.keyIdentifier, e.keyCode, e.which ];
-
-                while (key === undefined && possible.length > 0)
-                {
-                    key = possible.pop();
-                }
-
-                if (typeof key === "number" && ( 115 === key || 83 === key ) && (e.ctrlKey || e.metaKey) && !(e.altKey))
-                {
-                    ajaxSave(e);
-                    return false;
-                }
-                return true;
-
-            });
-
-        });
-    }
-
-
     function intAjaxModalLinks() {
         let ajaxModalLinks = document.querySelectorAll('a[data-ajax-modal-url]');
         [].forEach.call(ajaxModalLinks, function (link) {
---
+++ EMS/admin-ui-bundle/assets/js/core/plugins/form.js
@@ -0,0 +1,53 @@
+import $ from 'jquery'
+import ajaxRequest from '../components/AjaxRequest'
+
+class Form {
+  load (target) {
+    this.initAjaxSave(target)
+  }
+
+  initAjaxSave (target) {
+    $(target).find('button[data-ajax-save-url]').each(function () {
+      const button = $(this)
+      const form = button.closest('form')
+
+      const ajaxSave = function (event) {
+        event.preventDefault()
+
+        const formContent = form.serialize()
+        ajaxRequest.post(button.data('ajax-save-url'), formContent)
+          .success(function (message) {
+            let response = message
+            if (!(response instanceof Object)) {
+              response = $.parseJSON(message)
+            }
+
+            $(form).find('.has-error').removeClass('has-error')
+
+            $(response.errors).each(function (index, item) {
+              $('#' + item.propertyPath).parent().addClass('has-error')
+            })
+          })
+      }
+
+      button.on('click', ajaxSave)
+
+      $(document).keydown(function (e) {
+        let key
+        const possible = [e.key, e.keyIdentifier, e.keyCode, e.which]
+
+        while (key === undefined && possible.length > 0) {
+          key = possible.pop()
+        }
+
+        if (typeof key === 'number' && (key === 115 || key === 83) && (e.ctrlKey || e.metaKey) && !(e.altKey)) {
+          ajaxSave(e)
+          return false
+        }
+        return true
+      })
+    })
+  }
+}
+
+export default Form

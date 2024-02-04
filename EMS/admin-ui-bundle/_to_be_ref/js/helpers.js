ce40c40d3bb149168fe9a4b0354502856b1e50d7
Mathieu De Keyzer <me@theus.be>
2024-02-04 16:10:24 +0100

    ref: request notification buttons

--- EMS/admin-ui-bundle/_to_be_ref/js/helpers.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-
-window.requestNotification = function(element, tId, envName, ctId, id){
-    const data = { templateId : tId, environmentName : envName, contentTypeId : ctId, ouuid : id};
-    window.ajaxRequest.post(element.getAttribute("data-url") , data, 'modal-notifications');
-};
--- EMS/admin-ui-bundle/assets/js/core/plugins/button.js
+++ EMS/admin-ui-bundle/assets/js/core/plugins/button.js
@@ -1,9 +1,11 @@
 import $ from 'jquery'
+import ajaxRequest from '../components/ajaxRequest'
 
 export default class Button {
   load (target) {
     this.addRemoveButtonListeners(target)
     this.addDisabledButtonTreatListeners(target)
+    this.addRequestNotificationListeners(target)
   }
 
   addRemoveButtonListeners (target) {
@@ -28,6 +30,23 @@ export default class Button {
         const isDisabledAccept = this.value.length === 0
         form.elements.treat_notifications_accept.disabled = isDisabledAccept
         form.elements.treat_notifications_reject.disabled = !isDisabledAccept
+      })
+    }
+  }
+
+  addRequestNotificationListeners (target) {
+    const links = document.querySelectorAll('a.request-notification')
+    for (let i = 0; i < links.length; ++i) {
+      links[i].addEventListener('click', function (event) {
+        event.preventDefault()
+        const url = this.dataset.dataUrl
+        const data = {
+          templateId: this.dataset.templateId,
+          environmentName: this.dataset.environmentName,
+          contentTypeId: this.dataset.contentTypeId,
+          ouuid: this.dataset.ouuid
+        }
+        ajaxRequest.post(url, data, 'modal-notifications')
       })
     }
   }

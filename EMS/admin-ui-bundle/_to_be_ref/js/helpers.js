e184d53466aa1d43bc73cc98481c2a90e0787dbb
Mathieu De Keyzer <me@theus.be>
2024-02-04 13:53:17 +0100

    ref: color functions

--- EMS/admin-ui-bundle/_to_be_ref/js/helpers.js
+++ EMS/admin-ui-bundle/_to_be_ref/js/helpers.js
@@ -1,22 +1,5 @@
 'use strict';
 
-// color can be a hx string or an array of RGB values 0-255
-window.luma = function(color) {
-    const rgb = (typeof color === 'string') ? hexToRGBArray(color) : color;
-    return (0.2126 * rgb[0]) + (0.7152 * rgb[1]) + (0.0722 * rgb[2]); // SMPTE C, Rec. 709 weightings
-};
-
-window.hexToRGBArray = function(color) {
-    if (color.length === 3)
-        color = color.charAt(0) + color.charAt(0) + color.charAt(1) + color.charAt(1) + color.charAt(2) + color.charAt(2);
-    else if (color.length !== 6)
-        throw('Invalid hex color: ' + color);
-    let rgb = [];
-    for (let i = 0; i <= 2; i++)
-        rgb[i] = parseInt(color.substr(i * 2, 2), 16);
-    return rgb;
-};
-
 window.objectPickerListeners = function(objectPicker, maximumSelectionLength){
     const type = objectPicker.data('type');
     const dynamicLoading = objectPicker.data('dynamic-loading');

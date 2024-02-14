6120ba982514f6bf68974ec10341afad1546d074
David mattei <d.mattei89@gmail.com>
2024-02-14 11:17:34 +0100

    refactor: add AbstractQueryTableType.php
    So we can remove the last ajaxRoute inside the ReleaseController.php

--- EMS/core-bundle/src/Controller/ContentManagement/ReleaseController.php
+++ EMS/core-bundle/src/Controller/ContentManagement/ReleaseController.php
@@ -5,26 +5,22 @@ declare(strict_types=1);
 namespace EMS\CoreBundle\Controller\ContentManagement;
 
 use EMS\CoreBundle\Core\DataTable\DataTableFactory;
+use EMS\CoreBundle\DataTable\Type\Release\ReleaseNonMemberRevisionsDataTableType;
 use EMS\CoreBundle\DataTable\Type\Release\ReleaseOverviewDataTableType;
 use EMS\CoreBundle\DataTable\Type\Release\ReleasePickDataTableType;
 use EMS\CoreBundle\DataTable\Type\Release\ReleaseRevisionDataTableType;
 use EMS\CoreBundle\Entity\Release;
 use EMS\CoreBundle\Entity\Revision;
-use EMS\CoreBundle\Form\Data\QueryTable;
 use EMS\CoreBundle\Form\Data\TableAbstract;
-use EMS\CoreBundle\Form\Data\TemplateBlockTableColumn;
 use EMS\CoreBundle\Form\Form\ReleaseType;
 use EMS\CoreBundle\Form\Form\TableType;
-use EMS\CoreBundle\Helper\DataTableRequest;
 use EMS\CoreBundle\Routes;
-use EMS\CoreBundle\Service\ReleaseRevisionService;
 use EMS\CoreBundle\Service\ReleaseService;
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\ClickableInterface;
 use Symfony\Component\Form\Form;
 use Symfony\Component\Form\SubmitButton;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -35,22 +31,9 @@ final class ReleaseController extends AbstractController
     public function __construct(
         private readonly LoggerInterface $logger,
         private readonly ReleaseService $releaseService,
-        private readonly ReleaseRevisionService $releaseRevisionService,
         private readonly DataTableFactory $dataTableFactory,
         private readonly string $templateNamespace
     ) {
-    }
-
-    public function ajaxReleaseTableNonMemberRevisions(Request $request, Release $release): Response
-    {
-        $table = $this->getNonMemberRevisionsTable($release);
-        $dataTableRequest = DataTableRequest::fromRequest($request);
-        $table->resetIterator($dataTableRequest);
-
-        return $this->render("@$this->templateNamespace/datatable/ajax.html.twig", [
-            'dataTableRequest' => $dataTableRequest,
-            'table' => $table,
-        ], new JsonResponse());
     }
 
     public function index(Request $request): Response
@@ -205,7 +188,9 @@ final class ReleaseController extends AbstractController
 
     public function addRevisions(Request $request, Release $release): Response
     {
-        $table = $this->getNonMemberRevisionsTable($release);
+        $table = $this->dataTableFactory->create(ReleaseNonMemberRevisionsDataTableType::class, [
+            'release_id' => $release->getId(),
+        ]);
         $form = $this->createForm(TableType::class, $table);
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
@@ -245,22 +230,5 @@ final class ReleaseController extends AbstractController
             'form' => $form->createView(),
             'revision' => $revision,
         ]);
-    }
-
-    private function getNonMemberRevisionsTable(Release $release): QueryTable
-    {
-        $table = new QueryTable($this->templateNamespace, $this->releaseRevisionService, 'revisions-to-publish', $this->generateUrl(Routes::RELEASE_NON_MEMBER_REVISION_AJAX, ['release' => $release->getId()]), $release);
-        $table->setMassAction(true);
-        $table->setLabelAttribute('item_labelField');
-        $table->setIdField('emsLink');
-        $table->addColumn('release.revision.index.column.label', 'item_labelField');
-        $table->addColumn('release.revision.index.column.CT', 'content_type_singular_name');
-        $table->setSelected($release->getRevisionsOuuids());
-        $table->addColumnDefinition(new TemplateBlockTableColumn('release.revision.index.column.minRevId', 'minrevid', "@$this->templateNamespace/release/columns/revisions.html.twig"));
-        $table->addColumnDefinition(new TemplateBlockTableColumn('release.revision.index.column.maxRevId', 'maxrevid', "@$this->templateNamespace/release/columns/revisions.html.twig"));
-        $table->addTableAction(TableAbstract::ADD_ACTION, 'fa fa-plus', 'release.revision.actions.add', 'release.revision.actions.add_confirm');
-        $table->addDynamicItemPostAction(Routes::RELEASE_ADD_REVISION, 'release.revision.action.add', 'plus', 'release.revision.actions.add_confirm', ['release' => \sprintf('%d', $release->getId()), 'emsLinkToAdd' => 'emsLink']);
-
-        return $table;
     }
 }

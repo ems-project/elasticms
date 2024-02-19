62004a2a583742fde9e3da25805311d0fe9aa63b
Mathieu De Keyzer <me@theus.be>
2024-02-19 21:07:36 +0100

    fix: closeModalNotification and intAjaxModalLinks

--- EMS/admin-ui-bundle/assets/js/core/components/modal.js
+++ EMS/admin-ui-bundle/assets/js/core/components/modal.js
@@ -1,9 +1,13 @@
 'use strict'
+import $ from 'jquery'
 import queryString from '../helpers/queryString'
+import ajaxModal from '../../../js/core/helpers/ajaxModal'
 
 export default class Modal {
   constructor () {
     this.autoOpenModal()
+    this.closeModalNotification()
+    this.intAjaxModalLinks()
   }
 
   autoOpenModal () {
@@ -13,5 +17,28 @@ export default class Modal {
       const modal = window.bootstrap.Modal.getOrCreateInstance(modalElement)
       modal.show()
     }
+  }
+
+  closeModalNotification () {
+    $('#modal-notification-close-button').on('click', function () {
+      $('#modal-notifications .modal-body').empty()
+      $('#modal-notifications').modal('hide')
+    })
+  }
+
+  intAjaxModalLinks () {
+    const ajaxModalLinks = document.querySelectorAll('a[data-ajax-modal-url]');
+    [].forEach.call(ajaxModalLinks, function (link) {
+      link.onclick = (event) => {
+        ajaxModal.load({
+          url: event.target.dataset.ajaxModalUrl,
+          size: event.target.dataset.ajaxModalSize
+        }, (json) => {
+          if (Object.prototype.hasOwnProperty.call(json, 'success') && json.success === true) {
+            location.reload()
+          }
+        })
+      }
+    })
   }
 }

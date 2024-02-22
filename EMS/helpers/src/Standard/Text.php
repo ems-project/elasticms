3e6901325947d334973b494dc5c7db16e50a9057
Mathieu De Keyzer <me@theus.be>
2024-02-22 13:23:20 +0100

    refactor(bootstrap 5): remove selectpicker for select2 (#775)

--- EMS/helpers/src/Standard/Text.php
+++ EMS/helpers/src/Standard/Text.php
@@ -8,4 +8,31 @@ class Text
     {
         return \trim(\preg_replace('!\s+!', ' ', $content) ?? '');
     }
+
+    public static function humanize(string $str): string
+    {
+        $str = \trim(\strtolower($str));
+        $str = \preg_replace('/\_/', ' ', $str);
+
+        if (\is_string($str)) {
+            $str = \preg_replace('/[^a-z0-9\s+\-]/', '', $str);
+        }
+        if (\is_string($str)) {
+            $str = \preg_replace('/\s+/', ' ', $str);
+        }
+        if (\is_string($str)) {
+            $str = \preg_replace('/\-/', ' ', $str);
+        }
+        if (\is_string($str)) {
+            $str = \explode(' ', $str);
+        }
+
+        if (!\is_array($str)) {
+            throw new \RuntimeException('Humanize failed!');
+        }
+
+        $str = \array_map('ucwords', $str);
+
+        return \implode(' ', $str);
+    }
 }

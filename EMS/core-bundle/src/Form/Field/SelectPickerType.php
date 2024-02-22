f5f596ad31d13b05fec7ad0d3e72bcf33b186555
Mathieu De Keyzer <me@theus.be>
2024-02-22 11:15:03 +0100

    ref: SelectPickerType::humanize -> Text::humanize

--- EMS/core-bundle/src/Form/Field/SelectPickerType.php
+++ EMS/core-bundle/src/Form/Field/SelectPickerType.php
@@ -15,31 +15,4 @@ class SelectPickerType extends ChoiceType
     {
         return 'selectpicker';
     }
-
-    public static function humanize(string $str): string
-    {
-        $str = \trim(\strtolower($str));
-        $str = \preg_replace('/\_/', ' ', $str);
-
-        if (\is_string($str)) {
-            $str = \preg_replace('/[^a-z0-9\s+\-]/', '', $str);
-        }
-        if (\is_string($str)) {
-            $str = \preg_replace('/\s+/', ' ', $str);
-        }
-        if (\is_string($str)) {
-            $str = \preg_replace('/\-/', ' ', $str);
-        }
-        if (\is_string($str)) {
-            $str = \explode(' ', $str);
-        }
-
-        if (!\is_array($str)) {
-            throw new \RuntimeException('Humanize failed!');
-        }
-
-        $str = \array_map('ucwords', $str);
-
-        return \implode(' ', $str);
-    }
 }

5e09b65715cb45b6ca02fec4ad0b2a83f88affd9
Mathieu De Keyzer <me@theus.be>
2024-02-29 07:00:34 +0100

    ref: loading cke5 in iframe => it's a fail

--- EMS/admin-ui-bundle/assets/js/editor.js
+++ EMS/admin-ui-bundle/assets/js/editor.js
@@ -1,3 +1,77 @@
 import '../css/iframe.scss'
 
-console.log('editor')
+import { ClassicEditor } from '@ckeditor/ckeditor5-editor-classic'
+import { Essentials } from '@ckeditor/ckeditor5-essentials'
+import { Autoformat } from '@ckeditor/ckeditor5-autoformat'
+import { Bold, Italic } from '@ckeditor/ckeditor5-basic-styles'
+import { BlockQuote } from '@ckeditor/ckeditor5-block-quote'
+import { Heading } from '@ckeditor/ckeditor5-heading'
+import { Link } from '@ckeditor/ckeditor5-link'
+import { List } from '@ckeditor/ckeditor5-list'
+import { Paragraph } from '@ckeditor/ckeditor5-paragraph'
+
+let loaded = false
+
+function loadWYSIWYG() {
+    ClassicEditor
+        .create(document.getElementById('wysiwyg-content'), {
+            plugins: [
+                Essentials,
+                Autoformat,
+                Bold,
+                Italic,
+                BlockQuote,
+                Heading,
+                Link,
+                List,
+                Paragraph
+            ],
+            toolbar: [
+                'heading',
+                'bold',
+                'italic',
+                'link',
+                'bulletedList',
+                'numberedList',
+                'blockQuote',
+                'undo',
+                'redo'
+            ]
+        })
+        .then(editor => {
+            console.log(editor)
+        })
+        .catch(error => {
+            console.error(error)
+        })
+}
+
+const resizeFct = function () {
+    if (!loaded) {
+        return
+    }
+    window.parent.postMessage('resize', document.body.dataset.targetOrigine)
+}
+const startReady = function () {
+    if (loaded) {
+        return
+    }
+    window.parent.postMessage('ready', document.body.dataset.targetOrigine)
+    setTimeout(startReady, 1000)
+}
+
+window.addEventListener('message', function (event) {
+    if (event.source !== window.parent) {
+        console.log('Not a parent\'s message')
+        return
+    }
+    loaded = true
+    document.getElementById('wysiwyg-content').insertAdjacentHTML('afterbegin', event.data)
+    loadWYSIWYG()
+    resizeFct()
+})
+
+window.addEventListener('resize', resizeFct)
+window.addEventListener('redraw', resizeFct)
+window.addEventListener('load', resizeFct)
+startReady()

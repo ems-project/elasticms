5b4e1e1fb9adee1d07b8f51b3ee6f4399d91a945
Mathieu De Keyzer <me@theus.be>
2024-03-06 14:09:10 +0100

    feat: set style set assets with the just uploaded archives

---
+++ EMS/common-bundle/src/Contracts/CoreApi/Endpoint/Admin/ConfigTypes.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EMS\CommonBundle\Contracts\CoreApi\Endpoint\Admin;
+
+class ConfigTypes
+{
+    public const WYSIWYG_STYLE_SET = 'wysiwyg-style-set';
+}
---
+++ EMS/helpers/src/Html/MimeTypes.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EMS\Helpers\Html;
+
+class MimeTypes
+{
+    public const APPLICATION_ZIP = 'application/zip';
+}

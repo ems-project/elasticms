a9d5522e7e30ac420cf64cac62f8cabcf32ea810
Mathieu De Keyzer <me@theus.be>
2024-04-15 07:59:56 +0200

    poc: cropperjs

--- EMS/admin-ui-bundle/assets/js/core/plugins/image.js
+++ EMS/admin-ui-bundle/assets/js/core/plugins/image.js
@@ -1,13 +1,39 @@
 import $ from 'jquery'
 import 'jquery-lazyload'
+import 'cropperjs/dist/cropper.css'
+import '../../../css/core/plugins/cropper.scss'
+import Cropper from 'cropperjs'
 
 class Image {
   load (target) {
+    this.loadLazy(target)
+    this.loadCropper(target)
+  }
+
+  loadLazy (target) {
     const query = $(target)
     query.find('img.lazy').show().lazyload({
       effect: 'fadeIn',
       threshold: 200
     })
+  }
+
+  loadCropper (target) {
+    const images = target.querySelectorAll('img.ems-cropper')
+    for (let i = 0; i < images.length; ++i) {
+      const cropper = new Cropper(images[i], {
+        aspectRatio: 16 / 9,
+        crop (event) {
+          console.log(event.detail.x)
+          console.log(event.detail.y)
+          console.log(event.detail.width)
+          console.log(event.detail.height)
+          console.log(event.detail.rotate)
+          console.log(event.detail.scaleX)
+          console.log(event.detail.scaleY)
+        }
+      })
+    }
   }
 }
 
--- EMS/core-bundle/src/Entity/Form/AssetEntity.php
+++ EMS/core-bundle/src/Entity/Form/AssetEntity.php
@@ -2,6 +2,8 @@
 
 namespace EMS\CoreBundle\Entity\Form;
 
+use EMS\CommonBundle\Helper\EmsFields;
+
 class AssetEntity
 {
     private string $filename;
@@ -43,5 +45,14 @@ class AssetEntity
     public function setConfig(array $config): void
     {
         $this->config = $config;
+    }
+
+    public function getFile(): array
+    {
+        return [
+            EmsFields::CONTENT_FILE_NAME_FIELD => $this->filename,
+            EmsFields::CONTENT_FILE_HASH_FIELD => $this->hash,
+            EmsFields::CONTENT_MIME_TYPE_FIELD => $this->config[EmsFields::CONTENT_MIME_TYPE_FIELD_] ?? 'application/bin',
+        ];
     }
 }

488723a948f1b4ba984599ff9524eb057a821291
David mattei <d.mattei89@gmail.com>
2024-04-17 20:24:24 +0200

    refactor: remove AuthenticationEntryPointInterface for SamlAuthenticator
    Not needed we have a FirewallEntryPoint service, which will decide the authenticator to call.

--- EMS/client-helper-bundle/src/Security/Sso/Saml/SamlAuthenticator.php
+++ EMS/client-helper-bundle/src/Security/Sso/Saml/SamlAuthenticator.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace EMS\ClientHelperBundle\Security\Sso\Saml;
 
 use EMS\ClientHelperBundle\Security\Sso\User\SamlUser;
-use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
@@ -15,12 +14,10 @@ use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
 use Symfony\Component\Security\Http\Authenticator\Passport\SelfValidatingPassport;
-use Symfony\Component\Security\Http\EntryPoint\AuthenticationEntryPointInterface;
-use Symfony\Component\Security\Http\EntryPoint\Exception\NotAnEntryPointException;
 use Symfony\Component\Security\Http\HttpUtils;
 use Symfony\Component\Security\Http\Util\TargetPathTrait;
 
-class SamlAuthenticator extends AbstractAuthenticator implements AuthenticationEntryPointInterface
+class SamlAuthenticator extends AbstractAuthenticator
 {
     use TargetPathTrait;
 
@@ -29,15 +26,6 @@ class SamlAuthenticator extends AbstractAuthenticator implements AuthenticationE
         private readonly SamlConfig $samlConfig,
         private readonly SamlAuthFactory $samlAuthFactory,
     ) {
-    }
-
-    public function start(Request $request, AuthenticationException $authException = null): Response
-    {
-        if (!$this->samlConfig->isEnabled()) {
-            throw new NotAnEntryPointException();
-        }
-
-        return new RedirectResponse($this->httpUtils->generateUri($request, SamlConfig::ROUTE_LOGIN));
     }
 
     public function supports(Request $request): ?bool

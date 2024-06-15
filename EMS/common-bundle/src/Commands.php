ef592ec63d0d86a615336efee66096f36f4a6b0a
Mathieu De Keyzer <me@theus.be>
2024-06-15 17:09:46 +0200

    feat: ems:file-structure:publish

--- EMS/common-bundle/src/Commands.php
+++ EMS/common-bundle/src/Commands.php
@@ -11,4 +11,5 @@ class Commands
     final public const CLEAR_LOGS = 'ems:logs:clear';
     final public const ADMIN_COMMAND = 'ems:admin:command';
     final public const ADMIN_NEXT_JOB = 'ems:admin:next-job';
+    final public const FILE_STRUCTURE_PUBLISH = 'ems:file-structure:publish';
 }

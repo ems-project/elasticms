ac43cb139ddf1a21a014c5c3abcdbefc05997d81
Mathieu De Keyzer <me@theus.be>
2024-06-15 22:25:09 +0200

    ref: AbstractFileStructureCommand.php

--- EMS/common-bundle/src/Command/FileStructure/FileStructurePublishCommand.php
+++ EMS/common-bundle/src/Command/FileStructure/FileStructurePublishCommand.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace EMS\CommonBundle\Command\FileStructure;
 
 use EMS\CommonBundle\Commands;
-use EMS\CommonBundle\Common\Command\AbstractCommand;
 use EMS\CommonBundle\Common\File\FileStructure\FileStructureClientInterface;
 use EMS\CommonBundle\Common\File\FileStructure\S3Client;
 use EMS\CommonBundle\Common\PropertyAccess\PropertyAccessor;
@@ -20,74 +19,49 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class FileStructurePublishCommand extends AbstractCommand
+class FileStructurePublishCommand extends AbstractFileStructureCommand
 {
     protected static $defaultName = Commands::FILE_STRUCTURE_PUBLISH;
     public const ARGUMENT_INDEX = 'index';
-    public const ARGUMENT_IDENTIFIER = 'identifier';
     public const ARGUMENT_TARGET = 'target';
-    public const OPTION_TERM_FIELD = 'term-field';
-    public const OPTION_STRUCTURE_FIELD = 'structure-field';
     public const OPTION_S3_CREDENTIAL = 's3-credential';
-    private string $index;
-    private string $identifier;
-    private string $termField;
-    private string $structureField;
     private string $target;
     private ?string $s3Credential;
+    private string $index;
 
-    public function __construct(private readonly ElasticaService $elasticaService, private readonly StorageManager $storageManager)
+    public function __construct(ElasticaService $elasticaService, private readonly StorageManager $storageManager)
     {
-        parent::__construct();
+        parent::__construct($elasticaService);
     }
 
     protected function configure(): void
     {
+        parent::configure();
         $this->setDescription('Publish a json encoded file structure into a S3 bucket');
         $this->addArgument(self::ARGUMENT_INDEX, InputArgument::REQUIRED, 'Elasticsearch index');
-        $this->addArgument(self::ARGUMENT_IDENTIFIER, InputArgument::REQUIRED, 'Document identifier');
         $this->addArgument(self::ARGUMENT_TARGET, InputArgument::REQUIRED, 'Target (folder or bucket)');
-        $this
-            ->addOption(self::OPTION_TERM_FIELD, null, InputOption::VALUE_OPTIONAL, 'Term field used to get the document', '_id')
-            ->addOption(self::OPTION_STRUCTURE_FIELD, null, InputOption::VALUE_OPTIONAL, 'Document field path containing the JSON menu nested structure', '[structure]')
-            ->addOption(self::OPTION_S3_CREDENTIAL, null, InputOption::VALUE_OPTIONAL, 'S3 credential in a JSON format')
-        ;
+        $this->addOption(self::OPTION_S3_CREDENTIAL, null, InputOption::VALUE_OPTIONAL, 'S3 credential in a JSON format');
     }
 
     protected function initialize(InputInterface $input, OutputInterface $output): void
     {
         parent::initialize($input, $output);
         $this->index = $this->getArgumentString(self::ARGUMENT_INDEX);
-        $this->identifier = $this->getArgumentString(self::ARGUMENT_IDENTIFIER);
         $this->target = $this->getArgumentString(self::ARGUMENT_TARGET);
-        $this->termField = $this->getOptionString(self::OPTION_TERM_FIELD);
-        $this->structureField = $this->getOptionString(self::OPTION_STRUCTURE_FIELD);
         $this->s3Credential = $this->getOptionStringNull(self::OPTION_S3_CREDENTIAL);
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $this->io->title('File Structure - Publish');
-
-        $query = $this->elasticaService->getTermsQuery($this->termField, [$this->identifier]);
-        $search = $this->elasticaService->generateSearch([$this->index], $query);
-        $result = $this->elasticaService->search($search);
-        $result = $result->getResults();
-        if (0 === \count($result)) {
-            $this->io->warning(\sprintf('Document %s=%s not found in index %s', $this->termField, $this->identifier, $this->index));
-
+        $document = $this->getDocument($this->index);
+        if (null === $document) {
             return self::EXECUTE_ERROR;
         }
 
-        if (\count($result) > 1) {
-            $this->io->warning(\sprintf('%d documents found for %s=%s in index %s', \count($result), $this->termField, $this->identifier, $this->index));
-
-            return self::EXECUTE_ERROR;
-        }
-        $document = $result[0];
         $propertyAccessor = PropertyAccessor::createPropertyAccessor();
         $hash = Type::string($propertyAccessor->getValue($document->getData(), \sprintf('[%s]', EMSSource::FIELD_HASH)));
-        $structureJson = Type::string($propertyAccessor->getValue($document->getData(), $this->structureField));
+        $structureJson = Type::string($propertyAccessor->getValue($document->getData(), "[$this->structureField]"));
         $structure = JsonMenuNested::fromStructure($structureJson);
         $client = $this->getClient();
         $client->initSync($this->identifier, $hash);

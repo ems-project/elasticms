f9bd8f0c1c3181c6253e995bc24d07b7e908de75
Mathieu De Keyzer <me@theus.be>
2024-06-23 10:54:24 +0200

    ref: Archive (document import from zip file) is using TempDirectory

---
+++ EMS/helpers/src/File/TempDirectory.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EMS\Helpers\File;
+
+use Symfony\Component\Filesystem\Filesystem;
+
+class TempDirectory
+{
+    private const PREFIX = 'EMS_temp_dir_';
+    private Filesystem $filesystem;
+
+    private function __construct(public readonly string $path)
+    {
+        $this->filesystem = new Filesystem();
+        $this->filesystem->remove($this->path);
+        $this->filesystem->mkdir($this->path);
+    }
+
+    public function __destruct()
+    {
+        $this->filesystem->remove($this->path);
+    }
+
+    public static function create(): self
+    {
+        if (!$path = \tempnam(\sys_get_temp_dir(), self::PREFIX)) {
+            throw new \RuntimeException(\sprintf('Could not create temp directory in "%s"', \sys_get_temp_dir()));
+        }
+
+        return new self($path);
+    }
+
+    public function exists(): bool
+    {
+        return \is_dir($this->path);
+    }
+}

ae69ed8e3b02c718f12ecaa6cdd4e79771717b76
David mattei <d.mattei89@gmail.com>
2024-07-01 14:16:08 +0200

    refactor(emsch/api): common index not extend from draft
    When we call the core index endpoint we always will have an ouuid and revisionId.

--- EMS/common-bundle/src/Common/CoreApi/Endpoint/Data/Index.php
+++ EMS/common-bundle/src/Common/CoreApi/Endpoint/Data/Index.php
@@ -1,7 +1,31 @@
 <?php
 
+declare(strict_types=1);
+
 namespace EMS\CommonBundle\Common\CoreApi\Endpoint\Data;
 
-class Index extends Draft
+use EMS\CommonBundle\Common\CoreApi\Result;
+
+class Index
 {
+    private readonly int $id;
+    private readonly string $ouuid;
+
+    public function __construct(Result $result)
+    {
+        $data = $result->getData();
+
+        $this->id = $data['revision_id'];
+        $this->ouuid = $data['ouuid'];
+    }
+
+    public function getRevisionId(): int
+    {
+        return $this->id;
+    }
+
+    public function getOuuid(): string
+    {
+        return $this->ouuid;
+    }
 }

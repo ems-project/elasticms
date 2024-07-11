3adfeb366ca9720ff99d33edebdd063dd21651e2
David mattei <d.mattei89@gmail.com>
2024-07-11 20:57:10 +0200

    refactor: overwrite render method in controller trait

--- EMS/core-bundle/src/Controller/CoreControllerTrait.php
+++ EMS/core-bundle/src/Controller/CoreControllerTrait.php
@@ -6,6 +6,9 @@ namespace EMS\CoreBundle\Controller;
 
 use Symfony\Component\Form\Form;
 use Symfony\Component\Form\FormInterface;
+use Symfony\Component\HttpFoundation\Response;
+
+use function Symfony\Component\String\u;
 
 trait CoreControllerTrait
 {
@@ -18,5 +21,19 @@ trait CoreControllerTrait
         $clickedButton = $form->getClickedButton();
 
         return $clickedButton instanceof FormInterface ? $clickedButton->getName() : null;
+    }
+
+    /**
+     * @param array<string, mixed> $parameters
+     */
+    protected function render(string $view, array $parameters = [], ?Response $response = null): Response
+    {
+        $templateNamespace = parent::getParameter('ems_core.template_namespace');
+
+        if (\is_string($templateNamespace) && 'EMSCore' !== $templateNamespace) {
+            $view = u($view)->replaceMatches('/^@EMSCore/', '@'.$templateNamespace)->toString();
+        }
+
+        return parent::render($view, $parameters, $response);
     }
 }

2b7c79edcb1a4406feeeefa3ae70dd92c2c01d78
David mattei <d.mattei89@gmail.com>
2024-07-15 22:38:29 +0200

    refactor(common/log): use translateMessage in LocalizedLogger
    In the past we already created a common LocalizedLogger, but now we want to be able to translated with translatable messages.
    
    Because then the sf crawler can scan the code for keys and detect unused keys.

--- EMS/common-bundle/src/Common/Log/LocalizedLogger.php
+++ EMS/common-bundle/src/Common/Log/LocalizedLogger.php
@@ -7,14 +7,38 @@ namespace EMS\CommonBundle\Common\Log;
 use EMS\CommonBundle\Contracts\Log\LocalizedLoggerInterface;
 use Psr\Log\AbstractLogger;
 use Psr\Log\LoggerInterface;
+use Symfony\Component\Translation\TranslatableMessage;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
 class LocalizedLogger extends AbstractLogger implements LocalizedLoggerInterface
 {
     private const PATTERN = '/%(?<parameter>(_|)[[:alnum:]_]*)%/m';
 
-    public function __construct(private readonly LoggerInterface $logger, private readonly TranslatorInterface $translator, private readonly string $translationDomain)
+    public function __construct(
+        private readonly LoggerInterface $logger,
+        private readonly TranslatorInterface $translator,
+        private readonly string $translationDomain
+    ) {
+    }
+
+    public function message(string $level, TranslatableMessage $message, array $context = []): void
     {
+        $this->logger->log($level, $message->trans($this->translator), $context);
+    }
+
+    public function messageError(TranslatableMessage $message, array $context = []): void
+    {
+        $this->message('error', $message, $context);
+    }
+
+    public function messageWarning(TranslatableMessage $message, array $context = []): void
+    {
+        $this->message('warning', $message, $context);
+    }
+
+    public function messageNotice(TranslatableMessage $message, array $context = []): void
+    {
+        $this->message('notice', $message, $context);
     }
 
     /**

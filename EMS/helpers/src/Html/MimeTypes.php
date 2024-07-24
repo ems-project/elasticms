c45c95f1deba8c0e6db52d7971bcba5f12af4ddb
Mathieu De Keyzer <me@theus.be>
2024-07-24 14:40:48 +0200

    feat: image mime types

--- EMS/helpers/src/Html/MimeTypes.php
+++ EMS/helpers/src/Html/MimeTypes.php
@@ -8,4 +8,7 @@ enum MimeTypes: string
 {
     case APPLICATION_ZIP = 'application/zip';
     case APPLICATION_XML = 'application/xml';
+    case IMAGE_PNG = 'image/png';
+    case IMAGE_JPEG = 'image/jpeg';
+    case IMAGE_WEBP = 'image/webp';
 }

66ec9ade2b6cbe6bbfd5ddb5f40e5ff320830e98
David mattei <d.mattei89@gmail.com>
2024-10-03 16:22:59 +0200

    fix: if locale is null use 'en'

--- EMS/core-bundle/src/Core/ContentType/DataFieldFormOptions.php
+++ EMS/core-bundle/src/Core/ContentType/DataFieldFormOptions.php
@@ -7,7 +7,7 @@ namespace EMS\CoreBundle\Core\ContentType;
 class DataFieldFormOptions
 {
     public function __construct(
-        public readonly string $locale,
+        public readonly ?string $locale = 'en',
         public readonly ?string $referredEmsId = null,
     ) {
     }

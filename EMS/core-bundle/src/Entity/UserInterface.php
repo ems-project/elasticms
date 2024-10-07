80b0df08c342e8bcef14ec2e280364f12b9274ad
David mattei <d.mattei89@gmail.com>
2024-10-07 19:47:05 +0200

    fix: rollback add getUserId on UserInterface

--- EMS/core-bundle/src/Entity/UserInterface.php
+++ EMS/core-bundle/src/Entity/UserInterface.php
@@ -6,8 +6,6 @@ use Doctrine\Common\Collections\Collection;
 
 interface UserInterface extends \Symfony\Component\Security\Core\User\UserInterface
 {
-    public function getId(): int;
-
     public function getCreated(): \DateTimeInterface;
 
     public function getModified(): \DateTimeInterface;

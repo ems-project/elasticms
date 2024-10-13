015a724ad103fbbe12ef805e5812d469c23e0812
David Mattei <david.mattei@sappico.be>
2024-10-13 19:00:35 +0200

    fix: move environment variables

--- doc/.vitepress/nav/sidebar.ts
+++ doc/.vitepress/nav/sidebar.ts
@@ -1,34 +1,55 @@
-import type { DefaultTheme } from 'vitepress'
+import type {DefaultTheme} from 'vitepress'
 
 const sidebar: DefaultTheme.SidebarItem[] = {
-  '/': [
-    {
-      text: 'Guide',
-      collapsed: true,
-      items: [
-        {text: 'Introduction', link: '/guide/introduction' },
-        {text: 'Getting Started', link: '/guide/getting-started'},
-      ]
-    },
-    {
-      text: 'Release',
-      collapsed: true,
-      items: [
-        {text: 'Upgrade 5.x', link: '/release/upgrade-5x'},
-        {text: 'Upgrade 4.x', link: '/release/upgrade-4x'},
-        {text: 'Upgrade', link: '/release/upgrade'},
-      ]
-    },
-    {
-      text: 'Develop',
-      collapsed: true,
-      items: [
-        {text: 'Monorepo', link: '/guide/develop/monorepo' },
-        {text: 'Contributing', link: '/guide/develop/contributing'},
-        {text: 'Setup environment', link: '/guide/develop/environment' },
-      ]
-    }
-  ]
+    '/': [
+        {
+            text: 'Guide',
+            collapsed: true,
+            items: [
+                {text: 'Introduction', link: '/guide/introduction'},
+                {text: 'Getting Started', link: '/guide/getting-started'},
+            ]
+        },
+        {
+            text: 'Release',
+            collapsed: true,
+            items: [
+                {text: 'Upgrade 5.x', link: '/release/upgrade-5x'},
+                {text: 'Upgrade 4.x', link: '/release/upgrade-4x'},
+                {text: 'Upgrade', link: '/release/upgrade'},
+            ]
+        },
+        {
+            text: 'EMS - Admin',
+            collapsed: true,
+            items: [
+                {text: 'Environment variables', link: '/app/admin/environment-variables'},
+            ]
+        },
+        {
+            text: 'EMS - Web',
+            collapsed: true,
+            items: [
+                {text: 'Environment variables', link: '/app/web/environment-variables'},
+            ]
+        },
+        {
+            text: 'EMS - CLI',
+            collapsed: true,
+            items: [
+                {text: 'Environment variables', link: '/app/cli/environment-variables'},
+            ]
+        },
+        {
+            text: 'Develop',
+            collapsed: true,
+            items: [
+                {text: 'Monorepo', link: '/develop/monorepo'},
+                {text: 'Contributing', link: '/develop/contributing'},
+                {text: 'Setup environment', link: '/develop/environment'},
+            ]
+        }
+    ]
 }
 
 export default sidebar;

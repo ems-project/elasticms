689688e4112fe2214e00c5b89879971a633a3b27
Mathieu De Keyzer <me@theus.be>
2024-10-14 09:32:28 +0200

    refactor(common/cli): file structure use new ems archive (#1043)

--- EMS/common-bundle/src/Command/FileStructure/FileStructurePublishCommand.php
+++ EMS/common-bundle/src/Command/FileStructure/FileStructurePublishCommand.php
@@ -5,44 +5,41 @@ declare(strict_types=1);
 namespace EMS\CommonBundle\Command\FileStructure;
 
 use EMS\CommonBundle\Commands;
+use EMS\CommonBundle\Common\Command\AbstractCommand;
 use EMS\CommonBundle\Common\File\FileStructure\FileStructureClientInterface;
 use EMS\CommonBundle\Common\File\FileStructure\S3Client;
-use EMS\CommonBundle\Common\PropertyAccess\PropertyAccessor;
-use EMS\CommonBundle\Elasticsearch\Document\EMSSource;
 use EMS\CommonBundle\Exception\FileStructureNotSyncException;
-use EMS\CommonBundle\Json\JsonMenuNested;
-use EMS\CommonBundle\Service\ElasticaService;
+use EMS\CommonBundle\Storage\Archive;
 use EMS\CommonBundle\Storage\StorageManager;
 use EMS\Helpers\Standard\Json;
-use EMS\Helpers\Standard\Type;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class FileStructurePublishCommand extends AbstractFileStructureCommand
+class FileStructurePublishCommand extends AbstractCommand
 {
     protected static $defaultName = Commands::FILE_STRUCTURE_PUBLISH;
-    public const ARGUMENT_INDEX = 'index';
+    public const ARGUMENT_ARCHIVE_HASH = 'hash';
     public const ARGUMENT_TARGET = 'target';
     public const OPTION_S3_CREDENTIAL = 's3-credential';
     public const OPTION_FORCE = 'force';
     private string $target;
     private ?string $s3Credential;
-    private string $index;
+    private string $archiveHash;
     private bool $force;
 
-    public function __construct(ElasticaService $elasticaService, private readonly StorageManager $storageManager)
+    public function __construct(private readonly StorageManager $storageManager)
     {
-        parent::__construct($elasticaService);
+        parent::__construct();
     }
 
     protected function configure(): void
     {
         parent::configure();
-        $this->setDescription('Publish a json encoded file structure into a S3 bucket');
-        $this->addArgument(self::ARGUMENT_INDEX, InputArgument::REQUIRED, 'Elasticsearch index');
-        $this->addArgument(self::ARGUMENT_TARGET, InputArgument::REQUIRED, 'Target (folder or bucket)');
+        $this->setDescription('Publish the file structure of an ElasticMS archive into a S3 bucket');
+        $this->addArgument(self::ARGUMENT_ARCHIVE_HASH, InputArgument::REQUIRED, 'Elasticsearch index');
+        $this->addArgument(self::ARGUMENT_TARGET, InputArgument::REQUIRED, 'Target (S3 bucket)');
         $this->addOption(self::OPTION_S3_CREDENTIAL, null, InputOption::VALUE_OPTIONAL, 'S3 credential in a JSON format');
         $this->addOption(self::OPTION_FORCE, null, InputOption::VALUE_NONE, 'The target is synchronize even if the target looks already synchronized or if the target looks out of sync');
     }
@@ -50,7 +47,7 @@ class FileStructurePublishCommand extends AbstractFileStructureCommand
     protected function initialize(InputInterface $input, OutputInterface $output): void
     {
         parent::initialize($input, $output);
-        $this->index = $this->getArgumentString(self::ARGUMENT_INDEX);
+        $this->archiveHash = $this->getArgumentString(self::ARGUMENT_ARCHIVE_HASH);
         $this->target = $this->getArgumentString(self::ARGUMENT_TARGET);
         $this->s3Credential = $this->getOptionStringNull(self::OPTION_S3_CREDENTIAL);
         $this->force = $this->getOptionBool(self::OPTION_FORCE);
@@ -59,17 +56,12 @@ class FileStructurePublishCommand extends AbstractFileStructureCommand
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $this->io->title('File Structure - Publish');
-        $document = $this->getDocument($this->index);
-        if (null === $document) {
-            return self::EXECUTE_ERROR;
-        }
 
-        $propertyAccessor = PropertyAccessor::createPropertyAccessor();
-        $hash = Type::string($propertyAccessor->getValue($document->getData(), \sprintf('[%s]', EMSSource::FIELD_HASH)));
-        $structureJson = Type::string($propertyAccessor->getValue($document->getData(), "[$this->structureField]"));
-        $structure = JsonMenuNested::fromStructure($structureJson);
+        $algo = $this->storageManager->getHashAlgo();
+        $archive = Archive::fromStructure($this->storageManager->getContents($this->archiveHash), $algo);
+
         $client = $this->getClient();
-        $client->initSync($this->identifier, $hash);
+        $client->initSync($this->archiveHash);
         try {
             if (!$this->force && $client->isUpToDate()) {
                 $this->io->writeln('Bucket is already up to date');
@@ -77,22 +69,15 @@ class FileStructurePublishCommand extends AbstractFileStructureCommand
                 return self::EXECUTE_SUCCESS;
             }
         } catch (FileStructureNotSyncException $e) {
-            $this->io->error(\sprintf('The file structure might contains manual changes, use the --force option: %s', $e->getMessage()));
+            $this->io->error(\sprintf('The file structure might contain manual changes, use the --force option: %s', $e->getMessage()));
 
             return self::EXECUTE_SUCCESS;
         }
 
-        $this->io->progressStart($structure->count());
-        foreach ($structure->getIterator() as $item) {
-            $path = \implode('/', $item->getPath(fn (JsonMenuNested $item) => $item->getLabel()));
-            switch ($item->getType()) {
-                case 'folder':
-                    $client->createFolder($path, $item->getLabel());
-                    break;
-                default:
-                    $stream = $this->storageManager->getStream(Type::string($propertyAccessor->getValue($item->getObject(), '[file][sha1]')));
-                    $client->createFile($path, $stream, Type::string($propertyAccessor->getValue($item->getObject(), '[file][mimetype]')));
-            }
+        $this->io->progressStart($archive->getCount());
+        foreach ($archive->iterator() as $item) {
+            $stream = $this->storageManager->getStream($item->hash);
+            $client->createFile($item->filename, $stream, $item->type);
             $this->io->progressAdvance();
         }
         $this->io->progressFinish();
--- docs/recipes/websites/file-structure.md
+++ docs/recipes/websites/file-structure.md
@@ -1,34 +1,28 @@
-# Synchronize a directory structure and publish into an S3 bucket
-
-First of all you must configure a [`directory`](directory.json) content type in an elasticms admin.
-
-Each document of that `directory` content type can contains a file structure.
-
-## Initialize a document
-
-Just click on `New Directory`and fill the Identifier field. `Demo` in this example. Not need to fill the `Structure` fields.
+# File structure
 
 ## Push a local folder to ElasticMS
 
 The following command is available in all ElacticMS applications (Admin, Web and CLI) but required that your are logged in first (`ems:admin:login`).
 
 ```shell
-php bin/console ems:file-structure:push demo ../../demo --term-field=identifier
+php bin/console ems:file-structure:push ../../demo
 ```
+This command will upload all files present in the ../../demo folder an give you an hash as output.
+That hash identify an ElasticMS Archive, it's a JSON containing the all files structure (with hash, filename, size and mimetype of all files).
 
 ## Update a local folder from ElasticMS
 
-
 ```shell
-php bin/console ems:file-structure:pull demo ../../demo --term-field=identifier
+php bin/console ems:file-structure:pull d3bb0298fd9a69743333fb25dbe6cdefdc834ff2 ../../demo
 ```
+Update the folder ../../demo by the content of the ElasticMS archive identified by the hash `d3bb0298fd9a69743333fb25dbe6cdefdc834ff2`.
 
 ## Publish a file structure to a S3 bucket
 
-There is another command that allows you to publish a `directory` document, for a revision publish in some  environment into a S3 bucket.
+There is another command that allows you to publish an ElasticMS archive into a S3 bucket.
 
 ```shell
-php bin/console ems:file-structure:publish demo website_preview website --term-field=identifier --s3-credential='{"version":"2006-03-01","credentials":{"key":"accesskey","secret":"secretkey"},"region":"us-east-1","endpoint":"http://localhost:9000","use_path_style_endpoint":true}'
+php bin/console ems:file-structure:publish d3bb0298fd9a69743333fb25dbe6cdefdc834ff2 website --term-field=identifier --s3-credential='{"version":"2006-03-01","credentials":{"key":"accesskey","secret":"secretkey"},"region":"us-east-1","endpoint":"http://localhost:9000","use_path_style_endpoint":true}'
 ```
 
 ## Publish your bucket as website

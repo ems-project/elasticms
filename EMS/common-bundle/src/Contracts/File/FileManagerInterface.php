0ea7d00ffe7d5ca4f38689101cd900c494faa3e5
Mathieu De Keyzer <me@theus.be>
2024-11-24 13:11:51 +0100

    fix: I was facing "Too many open files" error with HEADS_CHUNK_SIZE=1000

--- EMS/common-bundle/src/Contracts/File/FileManagerInterface.php
+++ EMS/common-bundle/src/Contracts/File/FileManagerInterface.php
@@ -8,7 +8,7 @@ use Psr\Http\Message\StreamInterface;
 
 interface FileManagerInterface
 {
-    public const HEADS_CHUNK_SIZE = 1000;
+    public const HEADS_CHUNK_SIZE = 512;
 
     public function downloadFile(string $hash): string;
 

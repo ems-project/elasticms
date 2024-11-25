04b3b99597f40000f6afefb15e203ed5aa979cdb
Mathieu De Keyzer <me@theus.be>
2024-11-25 15:44:54 +0100

    feat: ems:storage:load-archive-in-cache command

--- EMS/common-bundle/src/Commands.php
+++ EMS/common-bundle/src/Commands.php
@@ -11,6 +11,7 @@ class Commands
     final public const STATUS = 'ems:status';
     final public const CLEAR_LOGS = 'ems:logs:clear';
     final public const CLEAR_CACHE = 'ems:storage:clear-cache';
+    final public const LOAD_ARCHIVE_IN_CACHE = 'ems:storage:load-archive-in-cache';
     final public const ADMIN_COMMAND = 'ems:admin:command';
     final public const ADMIN_NEXT_JOB = 'ems:admin:next-job';
     final public const FILE_STRUCTURE_PUBLISH = 'ems:file-structure:publish';

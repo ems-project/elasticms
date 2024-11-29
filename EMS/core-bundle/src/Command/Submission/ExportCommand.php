440da558ec60f8e9a6849b4b01ea7c831195610e
Mathieu De Keyzer <me@theus.be>
2024-11-29 20:53:10 +0100

    feat: generate an excel

--- EMS/core-bundle/src/Command/Submission/ExportCommand.php
+++ EMS/core-bundle/src/Command/Submission/ExportCommand.php
@@ -3,6 +3,7 @@
 namespace EMS\CoreBundle\Command\Submission;
 
 use EMS\CommonBundle\Common\Command\AbstractCommand;
+use EMS\CommonBundle\Contracts\SpreadsheetGeneratorServiceInterface;
 use EMS\CommonBundle\Service\ExpressionService;
 use EMS\CoreBundle\Commands;
 use EMS\CoreBundle\Service\Form\Submission\FormSubmissionService;
@@ -16,12 +17,17 @@ class ExportCommand extends AbstractCommand
     protected static $defaultName = Commands::SUBMISSION_EXPORT;
     public const ARG_FIELDS = 'fields';
     public const OPTIONS_FILTER = 'filter';
+    public const OPTIONS_FORMAT = 'format';
     /** @var string[] */
     private array $fields;
     private ?string $filter;
+    private string  $format;
 
-    public function __construct(private readonly FormSubmissionService $formSubmissionService, private readonly ExpressionService $expressionService)
-    {
+    public function __construct(
+        private readonly FormSubmissionService $formSubmissionService,
+        private readonly ExpressionService $expressionService,
+        private readonly SpreadsheetGeneratorServiceInterface $spreadsheetGeneratorService,
+    ) {
         parent::__construct();
     }
 
@@ -35,9 +41,15 @@ class ExportCommand extends AbstractCommand
                 'Fields to export'
             )->addOption(
                 self::OPTIONS_FILTER,
-                'f',
+                null,
                 InputOption::VALUE_OPTIONAL,
                 'Expression to filter submissions'
+            )->addOption(
+                self::OPTIONS_FORMAT,
+                null,
+                InputOption::VALUE_OPTIONAL,
+                'File format of the export, xlsx or csv are supported',
+                'xlsx'
             );
     }
 
@@ -46,11 +58,13 @@ class ExportCommand extends AbstractCommand
         parent::initialize($input, $output);
         $this->fields = $this->getArgumentStringArray(self::ARG_FIELDS);
         $this->filter = $this->getOptionStringNull(self::OPTIONS_FILTER);
+        $this->format = $this->getOptionString(self::OPTIONS_FORMAT);
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $this->io->section('Export the form submissions');
+        $sheet = [[...$this->fields]];
 
         foreach ($this->formSubmissionService->getUnprocessed() as $submission) {
             $data = \array_merge([
@@ -62,10 +76,22 @@ class ExportCommand extends AbstractCommand
             if (null !== $this->filter && !$this->expressionService->evaluateToBool($this->filter, $data)) {
                 continue;
             }
+            $line = [];
             foreach ($this->fields as $field) {
-                echo $data[$field] ?? '';
+                $line[] = $data[$field] ?? '';
             }
+            $sheet[] = $line;
         }
+
+        $config = [
+            SpreadsheetGeneratorServiceInterface::SHEETS => [[
+                'rows' => $sheet,
+                'name' => 'submissions',
+            ]],
+            SpreadsheetGeneratorServiceInterface::CONTENT_FILENAME => 'submissions',
+            SpreadsheetGeneratorServiceInterface::WRITER => $this->format,
+        ];
+        $this->spreadsheetGeneratorService->generateSpreadsheetFile($config, 'submission.xlsx');
 
         return self::EXECUTE_SUCCESS;
     }

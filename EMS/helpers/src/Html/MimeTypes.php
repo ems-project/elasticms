986677b57a7b27601c1ea1acdfb420ca9df7347e
Mathieu De Keyzer <me@theus.be>
2024-12-07 19:00:41 +0100

    fix: XLIFF mimetype support

--- EMS/helpers/src/Html/MimeTypes.php
+++ EMS/helpers/src/Html/MimeTypes.php
@@ -7,6 +7,8 @@ namespace EMS\Helpers\Html;
 enum MimeTypes: string
 {
     case APPLICATION_ZIP = 'application/zip';
+    case APPLICATION_XLIFF_LEGACY = 'application/x-xliff+xml';
+    case APPLICATION_XLIFF = 'application/xliff+xml';
     case APPLICATION_XML = 'application/xml';
     case APPLICATION_OCTET_STREAM = 'application/octet-stream';
     case IMAGE_PNG = 'image/png';

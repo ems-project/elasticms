872356df46d30eeac0b9425f137bd4cbd8a53371
Mathieu De Keyzer <me@theus.be>
2025-01-03 13:54:50 +0100

    fix: test single file

--- EMS/client-helper-bundle/src/Helper/UserApi/FileService.php
+++ EMS/client-helper-bundle/src/Helper/UserApi/FileService.php
@@ -23,6 +23,9 @@ final class FileService
         $client = $this->client->createClient(['X-Auth-Token' => $request->headers->get('X-Auth-Token')]);
 
         $responses = [];
+        if ($request->files->count() !== 1) {
+            throw new  \RuntimeException(\sprintf('The upload file api only supports single file upload, %d files have been given', $request->files->count()));
+        }
         foreach ($request->files as $file) {
             $responses = $this->upload($client, $file);
         }

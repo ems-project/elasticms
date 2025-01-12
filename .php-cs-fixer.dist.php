a9e3557b116650c15011feed2d6ffa235ac6b53a
David Mattei <david.mattei@sappico.be>
2025-01-12 18:50:50 +0100

    feat(phpcs): enable declare_strict_types

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -18,6 +18,7 @@ return new PhpCsFixer\Config()
     ->setParallelConfig(PhpCsFixer\Runner\Parallel\ParallelConfigFactory::detect())
     ->setRules([
         '@Symfony' => true,
+        'declare_strict_types' => true,
         'phpdoc_separation' => ['skip_unlisted_annotations' => true],
         'native_function_invocation' => ['include' => ['@all']],
         'no_unused_imports' => true,

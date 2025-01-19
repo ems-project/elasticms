24c651b1c47c2b2472da9decac1ed8f705b504ef
David Mattei <david.mattei@sappico.be>
2025-01-19 23:12:36 +0100

    fix: remove zalas/phpunit-globals packages
    We do not need to set or overwrite env variables
    Removed the old not used DefaultControllerTest.php from admin app.

--- EMS/client-helper-bundle/tests/Integration/BootTest.php
+++ EMS/client-helper-bundle/tests/Integration/BootTest.php
@@ -4,16 +4,19 @@ declare(strict_types=1);
 
 namespace EMS\ClientHelperBundle\Tests\Integration;
 
+use EMS\ClientHelperBundle\Tests\Integration\App\Kernel;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
-/**
- * @env KERNEL_CLASS=EMS\ClientHelperBundle\Tests\Integration\App\Kernel
- */
 final class BootTest extends KernelTestCase
 {
     public function testKernelIsBooted()
     {
         self::bootKernel();
         $this->assertTrue(self::$booted);
+    }
+
+    protected static function getKernelClass(): string
+    {
+        return Kernel::class;
     }
 }

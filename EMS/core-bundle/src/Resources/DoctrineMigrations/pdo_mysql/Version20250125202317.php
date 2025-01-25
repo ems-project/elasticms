ac198df9e3d0318b2e6ddad07477b78f9ea6fdfd
Mathieu De Keyzer <me@theus.be>
2025-01-25 21:39:34 +0100

    feat: add an editor field to WysiwygProfile

---
+++ EMS/core-bundle/src/Resources/DoctrineMigrations/pdo_mysql/Version20250125202317.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Platforms\MySQLPlatform;
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+use EMS\CoreBundle\Entity\WysiwygProfile;
+
+final class Version20250125202317 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Add an editor field to the WYSIWYG profile entity';
+    }
+
+    #[\Override]
+    public function up(Schema $schema): void
+    {
+        $this->abortIf(
+            !$this->connection->getDatabasePlatform() instanceof MySQLPlatform,
+            "Migration can only be executed safely on '\Doctrine\DBAL\Platforms\MySQLPlatform'."
+        );
+        $this->addSql(\sprintf('ALTER TABLE wysiwyg_profile ADD editor VARCHAR(255) NOT NULL DEFAULT \'%s\'', WysiwygProfile::CKEDITOR4));
+    }
+
+    #[\Override]
+    public function down(Schema $schema): void
+    {
+        $this->abortIf(
+            !$this->connection->getDatabasePlatform() instanceof MySQLPlatform,
+            "Migration can only be executed safely on '\Doctrine\DBAL\Platforms\MySQLPlatform'."
+        );
+        $this->addSql('ALTER TABLE wysiwyg_profile DROP editor');
+    }
+}

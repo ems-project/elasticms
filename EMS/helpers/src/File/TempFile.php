ems-project/elasticms

commit 1e9e2868cac5badc34552ab7f5339d94ae9720fc

fix: remove unused static collector from tempFile
Tests creates tempFiles and also the code uses tempFiles
This results in a recursively deep self nested collector property on tempFile
It's not used so can be removed

EMS/helpers/src/File/TempFile.php
@@ -10,20 +10,9 @@ use Psr\Http\Message\StreamInterface;
 class TempFile
 {
     private const string PREFIX = 'EMS_temp_file_';
-    /** @var self[] */
-    private static array $collector = [];
 
     private function __construct(public readonly string $path)
     {
-        self::$collector[] = $this;
-    }
-
-    /**
-     * @return self[]
-     */
-    public static function getIterator(): array
-    {
-        return self::$collector;
     }
 
     public function __destruct()

commit 254226d5d44eaf8cfc8ac44b53ef1b9c841e9fec

fix(helper): prevent tempFile destructor from being called too early
When running file-reader:import got an error message:
File "/tmp/EMS_temp_file_no61lpabc1qq2doIThQ" does not exist.

Because the tempFile was destructed and deleted, caused by previous pr:
https://github.com/ems-project/elasticms/pull/1147/commits/1e9e2868cac5badc34552ab7f5339d94ae9720fc

Keeping a static public references prevents the destruct from beeing called too early

EMS/helpers/src/File/TempFile.php
@@ -9,10 +9,12 @@ use Psr\Http\Message\StreamInterface;
 
 class TempFile
 {
+    public static ?self $preventDestruct = null;
     private const string PREFIX = 'EMS_temp_file_';
 
     private function __construct(public readonly string $path)
     {
+        self::$preventDestruct = $this;
     }
 
     public function __destruct()
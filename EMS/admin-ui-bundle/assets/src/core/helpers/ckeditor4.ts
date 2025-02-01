6bf4af190b33db70bea44fc30f27e3c81f496a49
theus77 <me@theus.be>
2025-02-01 15:44:33 +0100

    feat: ckeditor4 added

---
+++ EMS/admin-ui-bundle/assets/src/core/helpers/ckeditor4.ts
@@ -0,0 +1,20 @@
+import {EditorRevisionOptions} from "./editorRevisionOptions.ts";
+
+import {EditorProfile} from "./editorProfile.ts";
+
+export default class Ckeditor4 {
+
+  private options: EditorRevisionOptions
+  private element: HTMLElement
+  private profile: EditorProfile
+  constructor(element: HTMLElement, options: EditorRevisionOptions | null, profile: EditorProfile) {
+    this.options = options ?? ({} as EditorRevisionOptions)
+    this.element = element
+    this.profile = profile
+    this.create(element)
+  }
+
+  private create(element: HTMLElement) {
+    console.log('cke4')
+  }
+}

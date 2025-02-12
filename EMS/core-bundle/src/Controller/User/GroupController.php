20dae421f1fa7e0a38b32f2f90ee52c6febb37e0
Zki49 <dockerce@2501L202621.smals-mvm.be>
2025-02-12 16:03:51 +0100

    feat: groupController::index

--- EMS/core-bundle/src/Controller/User/GroupController.php
+++ EMS/core-bundle/src/Controller/User/GroupController.php
@@ -1,8 +1,30 @@
 <?php
 
+declare(strict_types=1);
+
 namespace EMS\CoreBundle\Controller\User;
 
-class GroupController
-{
+use EMS\CommonBundle\Contracts\Log\LocalizedLoggerInterface;
+use EMS\CoreBundle\Core\DataTable\DataTableFactory;
+use EMS\CoreBundle\Core\Form\FieldTypeManager;
+use EMS\CoreBundle\Core\Form\FormManager;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
+class GroupController extends AbstractController
+{
+    public function __construct(
+        private readonly LocalizedLoggerInterface $logger,
+        private readonly FormManager $formManager,
+        private readonly FieldTypeManager $fieldTypeManager,
+        private readonly DataTableFactory $dataTableFactory,
+        private readonly string $templateNamespace,
+    ) {
+    }
+
+    public function index(Request $request): Response
+    {
+        return new Response('access to page', Response::HTTP_OK);
+    }
 }

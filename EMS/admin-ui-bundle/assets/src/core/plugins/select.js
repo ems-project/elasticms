99ddd6d2a4b45c9d70691cb16034015c5703d80a
Mathieu De Keyzer <me@theus.be>
2025-02-15 00:12:23 +0100

    ref: format

--- EMS/admin-ui-bundle/assets/src/core/plugins/select.js
+++ EMS/admin-ui-bundle/assets/src/core/plugins/select.js
@@ -1,4 +1,4 @@
-import "choices.js/public/assets/styles/choices.css"
+import 'choices.js/public/assets/styles/choices.css'
 import Choices from 'choices.js'
 
 class Select {
@@ -23,53 +23,55 @@ class Select {
   choices(target) {
     const elements = target.querySelectorAll('select.select2')
     for (let i = 0; i < elements.length; ++i) {
-        const element = elements[i]
-        const querySearchLabel = element.dataset.querySearchLabel
-        new Choices(element, {
-            placeholderValue: querySearchLabel ?? 'Search',
-            removeItemButton: true,
-            allowHTML: true,
-            callbackOnCreateTemplates: function(template, escapeForTemplate, getClassNames) {
-                return {
-                    item: ({ classNames }, data) => {
-                        let icon = ''
-                        if (data.element && data.element.dataset.icon) {
-                            icon = `<i class="${data.element.dataset.icon}"></i> `
-                        }
-                        return template(`
-                          <div class="${getClassNames(classNames.item).join(' ')} ${
-                                            getClassNames(data.highlighted
-                                                ? classNames.highlightedState
-                                                : classNames.itemSelectable).join(' ')
-                                        } ${
-                                            data.placeholder ? classNames.placeholder : ''
-                                        }" data-item data-id="${data.id}" data-value="${data.value}" ${
-                                            data.active ? 'aria-selected="true"' : ''
-                                        } ${data.disabled ? 'aria-disabled="true"' : ''}>
+      const element = elements[i]
+      const querySearchLabel = element.dataset.querySearchLabel
+      new Choices(element, {
+        placeholderValue: querySearchLabel ?? 'Search',
+        removeItemButton: true,
+        allowHTML: true,
+        callbackOnCreateTemplates: function (template, escapeForTemplate, getClassNames) {
+          return {
+            item: ({ classNames }, data) => {
+              let icon = ''
+              if (data.element && data.element.dataset.icon) {
+                icon = `<i class="${data.element.dataset.icon}"></i> `
+              }
+              return template(`
+                          <div class="${getClassNames(classNames.item).join(' ')} ${getClassNames(
+                            data.highlighted
+                              ? classNames.highlightedState
+                              : classNames.itemSelectable
+                          ).join(' ')} ${
+                            data.placeholder ? classNames.placeholder : ''
+                          }" data-item data-id="${data.id}" data-value="${data.value}" ${
+                            data.active ? 'aria-selected="true"' : ''
+                          } ${data.disabled ? 'aria-disabled="true"' : ''}>
                             ${icon}${data.label}
-                          </div>`);
-                    },
-                    choice: ({ classNames }, data) => {
-                        let icon = ''
-                        if (data.element && data.element.dataset.icon) {
-                            icon = `<i class="${data.element.dataset.icon}"></i> `
-                        }
-                        return template(`
-                          <div class="${getClassNames(classNames.item).join(' ')} ${getClassNames(classNames.itemChoice).join(' ')} ${
-                                            getClassNames(data.disabled ? classNames.itemDisabled : classNames.itemSelectable).join(' ')
-                                        }" data-select-text="${this.config.itemSelectText}" data-choice ${
-                                            data.disabled
-                                                ? 'data-choice-disabled aria-disabled="true"'
-                                                : 'data-choice-selectable'
-                                        } data-id="${data.id}" data-value="${data.value}" ${
-                                            data.groupId > 0 ? 'role="treeitem"' : 'role="option"'
-                                        }>
-                            ${icon}${data.label}
-                          </div>`);
-                    },
-                };
+                          </div>`)
             },
-        })
+            choice: ({ classNames }, data) => {
+              let icon = ''
+              if (data.element && data.element.dataset.icon) {
+                icon = `<i class="${data.element.dataset.icon}"></i> `
+              }
+              return template(`
+                          <div class="${getClassNames(classNames.item).join(' ')} ${getClassNames(classNames.itemChoice).join(' ')} ${getClassNames(
+                            data.disabled ? classNames.itemDisabled : classNames.itemSelectable
+                          ).join(
+                            ' '
+                          )}" data-select-text="${this.config.itemSelectText}" data-choice ${
+                            data.disabled
+                              ? 'data-choice-disabled aria-disabled="true"'
+                              : 'data-choice-selectable'
+                          } data-id="${data.id}" data-value="${data.value}" ${
+                            data.groupId > 0 ? 'role="treeitem"' : 'role="option"'
+                          }>
+                            ${icon}${data.label}
+                          </div>`)
+            }
+          }
+        }
+      })
     }
   }
 }

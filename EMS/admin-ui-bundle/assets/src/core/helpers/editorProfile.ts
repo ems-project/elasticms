f28f1b72ec2ef910bbd2f48a5764d12f6a98c0ac
Mathieu De Keyzer <me@theus.be>
2025-02-16 20:49:47 +0100

    chore: npm run format

--- EMS/admin-ui-bundle/assets/src/core/helpers/editorProfile.ts
+++ EMS/admin-ui-bundle/assets/src/core/helpers/editorProfile.ts
@@ -5,20 +5,28 @@ export class EditorProfile {
     config: object
   }[] = []
   config: {
-    emsBrowsers: undefined|{
-      browser_object: undefined|{
-        url: string
-        label: string
-      }
-      browser_file: undefined|{
-        url: string
-        label: string
-      }
-      browser_image: undefined|{
-        url: string
-        label: string
-      }
-    }
+    emsBrowsers:
+      | undefined
+      | {
+          browser_object:
+            | undefined
+            | {
+                url: string
+                label: string
+              }
+          browser_file:
+            | undefined
+            | {
+                url: string
+                label: string
+              }
+          browser_image:
+            | undefined
+            | {
+                url: string
+                label: string
+              }
+        }
   } = {
     emsBrowsers: undefined
   }

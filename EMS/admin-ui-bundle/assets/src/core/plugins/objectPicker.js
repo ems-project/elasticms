4e2472240e3b7a59906d6a55928378702035658c
Mathieu De Keyzer <me@theus.be>
2025-02-17 16:45:11 +0100

    fix: escape in attribute

--- EMS/admin-ui-bundle/assets/src/core/plugins/objectPicker.js
+++ EMS/admin-ui-bundle/assets/src/core/plugins/objectPicker.js
@@ -4,7 +4,7 @@ export default class ObjectPicker {
   load(target) {
     const elements = target.querySelectorAll('.objectpicker')
     for (let i = 0; i < elements.length; ++i) {
-        new ChoicePicker(elements[i])
+      new ChoicePicker(elements[i])
     }
   }
 }

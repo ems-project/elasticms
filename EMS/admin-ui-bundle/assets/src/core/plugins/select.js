9ff028eb7b69f6bb21038b255b6b6dce2fa46af7
Mathieu De Keyzer <me@theus.be>
2025-02-23 15:50:22 +0100

    fix: target is not an input

--- EMS/admin-ui-bundle/assets/src/core/plugins/select.js
+++ EMS/admin-ui-bundle/assets/src/core/plugins/select.js
@@ -13,7 +13,8 @@ class Select {
       checkboxesAll[i].addEventListener('change', function () {
         const targets = document.querySelectorAll(selector)
         for (let j = 0; j < targets.length; ++j) {
-          targets[j].checked = this.checked
+          const input = 'INPUT' === targets[j].tagName ? targets[j] : targets[j].querySelector('input')
+          input.checked = checkboxesAll[i].checked
         }
       })
     }

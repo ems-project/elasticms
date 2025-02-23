5bd0ac7afd348efe12b1e1635c15efa336ed49d4
Mathieu De Keyzer <me@theus.be>
2025-02-23 16:09:33 +0100

    chore: format

--- EMS/admin-ui-bundle/assets/src/core/plugins/select.js
+++ EMS/admin-ui-bundle/assets/src/core/plugins/select.js
@@ -13,7 +13,8 @@ class Select {
       checkboxesAll[i].addEventListener('change', function () {
         const targets = document.querySelectorAll(selector)
         for (let j = 0; j < targets.length; ++j) {
-          const input = 'INPUT' === targets[j].tagName ? targets[j] : targets[j].querySelector('input')
+          const input =
+            'INPUT' === targets[j].tagName ? targets[j] : targets[j].querySelector('input')
           input.checked = checkboxesAll[i].checked
         }
       })

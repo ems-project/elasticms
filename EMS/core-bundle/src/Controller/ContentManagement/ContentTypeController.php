33c1a0e7c35fc2a7b664ca7c29f9326f6e03da9d
Mathieu De Keyzer <me@theus.be>
2025-02-25 17:18:06 +0100

    refactor: structure

--- EMS/core-bundle/src/Controller/ContentManagement/ContentTypeController.php
+++ EMS/core-bundle/src/Controller/ContentManagement/ContentTypeController.php
@@ -523,6 +523,11 @@ class ContentTypeController extends AbstractController
         return $this->render("@$this->templateNamespace/contenttype/structure.html.twig", [
             'form' => $form->createView(),
             'contentType' => $contentType,
+            'title' => t('action.reorder', ['type' => 'content_type'], 'emsco-core'),
+            'subTitle' => t('type.title_sub', ['type' => 'content_type'], 'emsco-core'),
+            'breadcrumb' => Navigation::admin()->contentType($contentType)->add(
+                t('action.reorder', ['type' => 'content_type'], 'emsco-core')
+            ),
         ]);
     }
 

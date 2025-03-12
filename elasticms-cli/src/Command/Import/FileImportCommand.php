9c36670efd3471f407aade0646eac3f0c135447e
David mattei <d.mattei89@gmail.com>
2025-03-12 08:23:12 +0100

    feat(cli/import): add command for database import (#1253)

---
+++ elasticms-cli/src/Command/Import/FileImportCommand.php
@@ -0,0 +1,79 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\CLI\Command\Import;
+
+use App\CLI\Commands;
+use EMS\CommonBundle\Common\Admin\AdminHelper;
+use EMS\CommonBundle\Contracts\File\FileReaderInterface;
+use EMS\CommonBundle\Helper\MimeTypeHelper;
+use EMS\CommonBundle\Storage\StorageManager;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+#[AsCommand(
+    name: Commands::IMPORT_FILE,
+    description: 'Import an Excel file or a CSV file, one document per row.',
+    aliases: ['emscli:file-reader:import'],
+    hidden: false
+)]
+final class FileImportCommand extends AbstractImportCommand
+{
+    private const string ARGUMENT_FILE = 'file';
+
+    private string $file;
+
+    public function __construct(
+        private readonly FileReaderInterface $fileReader,
+        AdminHelper $adminHelper,
+        StorageManager $storageManager,
+    ) {
+        parent::__construct($adminHelper, $storageManager);
+    }
+
+    #[\Override]
+    protected function configure(): void
+    {
+        $this->addArgument(self::ARGUMENT_FILE, InputArgument::REQUIRED, 'File path (xlsx or csv)');
+        parent::configure();
+    }
+
+    #[\Override]
+    protected function initialize(InputInterface $input, OutputInterface $output): void
+    {
+        parent::initialize($input, $output);
+
+        $this->file = $this->getArgumentString(self::ARGUMENT_FILE);
+    }
+
+    #[\Override]
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        try {
+            $this->io->title('EMS CLI - Import - File');
+
+            $config = $this->getImportConfig();
+            $file = $this->getFile($this->file);
+            $mimeType = MimeTypeHelper::getInstance()->guessMimeType($file->getFilename());
+
+            $cells = $this->fileReader->readCells($file->getFilename(), [
+                'mime_type' => $mimeType,
+                'delimiter' => $config->delimiter,
+                'encoding' => $config->encoding,
+                'exclude_rows' => $config->excludeRows,
+                'limit' => $this->limit,
+            ]);
+
+            $this->import($config, $cells);
+
+            return self::EXECUTE_SUCCESS;
+        } catch (\Throwable $e) {
+            $this->io->error($e->getMessage());
+
+            return self::EXECUTE_ERROR;
+        }
+    }
+}

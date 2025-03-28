f5e8ffb0bfb77648bb15e5fe5efc1924f400d0fe
De Keyser <michaeldk.dekeyser@gmail.com>
2025-03-28 08:35:17 +0100

    feat(demo/js-translations): add support for pattern replacement (#1275)
    Co-authored-by: midk <michael.dekeyser@smals.be>

--- demo/src/js/translations.js
+++ demo/src/js/translations.js
@@ -1,3 +1,11 @@
-export default function t(key) {
-    return window.translations?.[key] || key;
+export default function t(key, replace = null) {
+    let translation = window.translations?.[key] || key;
+
+    if (replace !== null) {
+        Object.keys(replace).forEach(pattern => {
+            translation = translation.replace(pattern, replace[pattern]);
+        })
+    }
+
+    return translation;
 }

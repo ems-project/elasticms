9ecea43ac9b3004b409cfe7b5d709caa887578ca
De Keyser <michaeldk.dekeyser@gmail.com>
2025-03-28 08:45:30 +0100

    fix(admin-ui): fix ems font import (#1277)
    Co-authored-by: midk <michael.dekeyser@smals.be>
    Co-authored-by: David mattei <d.mattei89@gmail.com>

--- EMS/admin-ui-bundle/assets/vite.config.ts
+++ EMS/admin-ui-bundle/assets/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import inject from '@rollup/plugin-inject'
 import liveReload from 'vite-plugin-live-reload'
+import { resolve } from 'path';
 
 export default defineConfig({
   base: './',
@@ -42,7 +43,10 @@ export default defineConfig({
     })
   ],
   resolve: {
-    extensions: ['.js', '.ts']
+    extensions: ['.js', '.ts'],
+    alias: {
+      '@fonts': resolve('./public/fonts')
+    }
   },
   server: {
     host: '0.0.0.0',

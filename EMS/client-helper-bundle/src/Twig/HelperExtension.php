c9d4882c6e2b6681cf9120532e76bab2087fd2d6
David mattei <d.mattei89@gmail.com>
2025-04-04 10:48:47 +0200

    feat(web/twig): add 'ems_search_config_execute' twig function (#1289)

--- EMS/client-helper-bundle/src/Twig/HelperExtension.php
+++ EMS/client-helper-bundle/src/Twig/HelperExtension.php
@@ -37,6 +37,7 @@ final class HelperExtension extends AbstractExtension
             new TwigFunction('emsch_search_one', [ClientRequestRuntime::class, 'searchOne']),
             new TwigFunction('emsch_add_environment', [ClientRequestRuntime::class, 'addEnvironment']),
             new TwigFunction('emsch_search_config', [ClientRequestRuntime::class, 'searchConfig']),
+            new TwigFunction('emsch_search_config_execute', [ClientRequestRuntime::class, 'searchConfigExecute']),
             new TwigFunction('emsch_http_error', [ClientRequestRuntime::class, 'httpException']),
             new TwigFunction('emsch_asset', [AssetHelperRuntime::class, 'asset'], ['is_safe' => ['html']]),
             new TwigFunction('emsch_asset_redirect', [AssetHelperRuntime::class, 'assetRedirect']),

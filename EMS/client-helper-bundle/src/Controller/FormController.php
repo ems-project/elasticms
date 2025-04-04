8fe7b15a69ae9638d4ecd1b6e01ebcc31a31425f
David mattei <d.mattei89@gmail.com>
2025-04-04 09:43:12 +0200

    feat(client/emschForm): support validate block (#1287)

--- EMS/client-helper-bundle/src/Controller/FormController.php
+++ EMS/client-helper-bundle/src/Controller/FormController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace EMS\ClientHelperBundle\Controller;
 
+use EMS\ClientHelperBundle\Helper\Form\EmschFormBlock;
 use EMS\ClientHelperBundle\Helper\Form\EmschFormType;
 use EMS\ClientHelperBundle\Helper\Request\Handler;
 use Symfony\Component\Form\FormFactoryInterface;
@@ -13,9 +14,6 @@ use Symfony\Component\HttpFoundation\Response;
 
 readonly class FormController
 {
-    public const string BLOCK_SUCCESS_REDIRECT = 'emschFormSuccessRedirect';
-    public const string BLOCK_DATA = 'emschFormData';
-
     public function __construct(
         private Handler $handler,
         private FormFactoryInterface $formFactory,
@@ -26,7 +24,7 @@ readonly class FormController
     {
         $template = $this->handler->handle($request);
 
-        $data = $template->jsonBlock(self::BLOCK_DATA);
+        $data = $template->jsonBlock(EmschFormBlock::DATA->value);
 
         $form = $this->formFactory->create(EmschFormType::class, $data, ['template' => $template]);
         $form->handleRequest($request);
@@ -34,7 +32,7 @@ readonly class FormController
         if ($form->isSubmitted() && $form->isValid()) {
             $template->context()->append(['emschFormData' => $form->getData()]);
 
-            if ($redirect = $template->renderBlock(self::BLOCK_SUCCESS_REDIRECT)) {
+            if ($redirect = $template->renderBlock(EmschFormBlock::SUCCESS_REDIRECT->value)) {
                 return new RedirectResponse($redirect);
             }
         }

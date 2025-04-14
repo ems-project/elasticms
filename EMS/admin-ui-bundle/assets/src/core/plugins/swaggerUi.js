41408100bc8574ddbe7079a5e501a4ee8df4ca48
Zki49 <dockerce@2501L202621.smals-mvm.be>
2025-04-14 10:41:40 +0200

    feat: swaggerUi.js

---
+++ EMS/admin-ui-bundle/assets/src/core/plugins/swaggerUi.js
@@ -0,0 +1,12 @@
+import SwaggerUI from 'swagger-ui';
+import 'swagger-ui/dist/swagger-ui.css';
+
+export function SwaggerUi(){
+  document.addEventListener('DOMContentLoaded', () => {
+    if (!Object.hasOwn(document.body.dataset, 'openapi')) return;
+    SwaggerUI({
+      dom_id: '#swagger-ui',
+      url: document.body.dataset.openapi,
+    });
+  });
+}

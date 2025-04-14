a3ea56bfa4e9d39a2d9ed3c6d08b19d0791f4755
Zki49 <dockerce@2501L202621.smals-mvm.be>
2025-04-14 17:54:44 +0200

    feat: format json|html

--- EMS/core-bundle/src/Controller/Api/Admin/DocumentationController.php
+++ EMS/core-bundle/src/Controller/Api/Admin/DocumentationController.php
@@ -6,6 +6,7 @@ namespace EMS\CoreBundle\Controller\Api\Admin;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class DocumentationController extends AbstractController
@@ -13,8 +14,13 @@ class DocumentationController extends AbstractController
     public function __construct(private readonly string $templateNamespace)
     {
     }
-    public function getDocumentation(): Response
+    public function getDocumentation(Request $request): Response
     {
+        $format = $request->getRequestformat();
+        
+        if ($format === 'json') {
+            return new JsonResponse([]);
+        }
         return $this->render("@$this->templateNamespace/api/documentation.html.twig");
     }
 }

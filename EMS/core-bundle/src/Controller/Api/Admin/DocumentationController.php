53b2af9ced2a40d08ed02aa36886ed5e57c924d0
Zki49 <dockerce@2501L202621.smals-mvm.be>
2025-04-15 13:00:05 +0200

    feat: format html page

--- EMS/core-bundle/src/Controller/Api/Admin/DocumentationController.php
+++ EMS/core-bundle/src/Controller/Api/Admin/DocumentationController.php
@@ -8,19 +8,71 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\RouterInterface;
 
 class DocumentationController extends AbstractController
 {
-    public function __construct(private readonly string $templateNamespace)
+    public function __construct(private readonly string $templateNamespace,
+                                private readonly RouterInterface $router)
     {
     }
     public function getDocumentation(Request $request): Response
     {
-        $format = $request->getRequestformat();
-        
+        $format = $request->getRequestFormat();
+
         if ($format === 'json') {
-            return new JsonResponse([]);
+            $paths = [];
+
+            foreach ($this->getRoutes() as $name => $route) {
+                if (!str_starts_with($name, 'emsco_admin')) {
+                    continue;
+                }
+
+                $path = $route->getPath();
+                $methods = $route->getMethods();
+                $controller = $route->getDefault('_controller') ?? 'Not defined';
+
+                foreach ($methods as $method) {
+                    $paths[$path][strtolower($method)] = [
+                        'summary' => $name,
+                        'responses' => [
+                            '200' => [
+                                'description' => "Response for route $name",
+                            ],
+                        ],
+                    ];
+                }
+            }
+
+            $openApi = [
+                'openapi' => '3.0.0',
+                'info' => [
+                    'title' => 'Dynamically Generated API',
+                    'description' => 'OpenAPI documentation based on Symfony routes',
+                    'version' => '1.0.0',
+                ],
+                'paths' => $paths,
+            ];
+
+            return new JsonResponse($openApi);
         }
+
         return $this->render("@$this->templateNamespace/api/documentation.html.twig");
+    }
+
+
+    public function getRoutes(): array
+    {
+        $allRoutes = $this->router->getRouteCollection()->all();
+
+        $Routes = [];
+
+        foreach ($allRoutes as $name => $route) {
+            if (str_starts_with($name, 'emsco_admin')) {
+                $Routes[$name] = $route;
+            }
+        }
+
+        return $Routes;
     }
 }

5fd94f04b1ee7531625c7044893ab8b8b7845348
David mattei <d.mattei89@gmail.com>
2025-04-18 17:12:38 +0200

    refactor: clean ExtractAssetCommand (no breaking changes)

--- EMS/core-bundle/src/Command/ExtractAssetCommand.php
+++ EMS/core-bundle/src/Command/ExtractAssetCommand.php
@@ -10,76 +10,63 @@ use EMS\CoreBundle\Commands;
 use EMS\CoreBundle\Service\AssetExtractorService;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\Console\Attribute\AsCommand;
-use Symfony\Component\Console\Helper\ProgressBar;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Finder\Finder;
-use Symfony\Component\Finder\SplFileInfo;
 
 #[AsCommand(
     name: Commands::ASSET_EXTRACT,
-    description: 'Will extract data from all files found and load it in cache of the asset extractor service.',
-    hidden: false,
-    aliases: ['ems:asset:extract']
+    description: "Extracts data from all found files and loads it into the asset extractor service's cache",
+    aliases: ['ems:asset:extract'],
+    hidden: false
 )]
 class ExtractAssetCommand extends AbstractCommand
 {
-    public function __construct(protected LoggerInterface $logger, protected AssetExtractorService $extractorService, protected StorageManager $storageManager)
-    {
+    private const string ARG_PATH = 'path';
+    private const string ARG_NAME = 'name';
+
+    public function __construct(
+        protected LoggerInterface $logger,
+        protected AssetExtractorService $extractorService,
+        protected StorageManager $storageManager
+    ) {
         parent::__construct();
     }
 
     #[\Override]
     protected function configure(): void
     {
-        $this->addArgument(
-            'path',
-            InputArgument::REQUIRED,
-            'Path to the files to extract data from'
-        )
-            ->addArgument(
-                'name',
-                InputArgument::OPTIONAL,
-                'File pattern or file name i.e. *.pdf',
-                '*.*'
-            );
+        $this
+            ->addArgument(self::ARG_PATH, InputArgument::REQUIRED, 'Path to the files to extract data from')
+            ->addArgument(self::ARG_NAME, InputArgument::OPTIONAL, 'File pattern or file name i.e. *.pdf', '*.*')
+        ;
     }
 
     #[\Override]
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $name = $input->getArgument('name');
-        if (!\is_string($name)) {
-            throw new \RuntimeException('Unexpected name argument');
-        }
-        $path = $input->getArgument('path');
-        if (!\is_string($path)) {
-            throw new \RuntimeException('Unexpected path argument');
-        }
+        $path = $this->getArgumentString(self::ARG_PATH);
+        $name = $this->getArgumentString(self::ARG_NAME);
 
-        $finder = new Finder();
-        $fileIterator = $finder->in($path)->files()->name($name);
+        $files = new Finder()->in($path)->name($name);
 
-        $progress = new ProgressBar($output, $fileIterator->count());
+        $progress = $this->io->createProgressBar($files->count());
         $progress->start();
 
-        /** @var SplFileInfo $file */
-        foreach ($fileIterator as $file) {
-            $realPath = $file->getRealPath();
-            if (false === $realPath) {
+        foreach ($files as $file) {
+            if (false === $realPath = $file->getRealPath()) {
                 $progress->advance();
                 continue;
             }
 
             $hash = $this->storageManager->computeFileHash($realPath);
-            if (\is_string($file->getRealPath())) {
-                $this->extractorService->extractData($hash, $realPath);
-            }
+            $this->extractorService->extractMetaData($hash, $realPath);
+
             $progress->advance();
         }
         $progress->finish();
 
-        return 0;
+        return self::EXECUTE_SUCCESS;
     }
 }

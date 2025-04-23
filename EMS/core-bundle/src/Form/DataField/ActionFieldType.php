0c2588df1d4fbdbe939c8548cdddf0f920980875
David mattei <d.mattei89@gmail.com>
2025-04-23 09:55:58 +0200

    add new ActionFieldType
    No mapping, migration or extraOptions
    For restrictionOptions only keep minimum_role option

---
+++ EMS/core-bundle/src/Form/DataField/ActionFieldType.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EMS\CoreBundle\Form\DataField;
+
+use Symfony\Component\Form\FormBuilderInterface;
+
+class ActionFieldType extends DataFieldType
+{
+    #[\Override]
+    public function buildOptionsForm(FormBuilderInterface $builder, array $options): void
+    {
+        parent::buildOptionsForm($builder, $options);
+        $optionsForm = $builder->get('options');
+
+        $optionsForm
+            ->remove('mappingOptions')
+            ->remove('migrationOptions')
+            ->remove('extraOptions');
+
+        $restrictionOptions = $optionsForm->get('restrictionOptions');
+        $restrictionOptions->remove('mandatory')->remove('mandatory_if');
+    }
+
+    #[\Override]
+    public function getBlockPrefix(): string
+    {
+        return 'action';
+    }
+
+    #[\Override]
+    public static function getIcon(): string
+    {
+        return 'fa fa-cog';
+    }
+
+    #[\Override]
+    public function getLabel(): string
+    {
+        return 'Action field';
+    }
+
+    #[\Override]
+    public static function isContainer(): bool
+    {
+        return true;
+    }
+}

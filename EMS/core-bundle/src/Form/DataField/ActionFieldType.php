735a47234cb4baf2c6dc40fa648b2f223aa95274
David mattei <d.mattei89@gmail.com>
2025-04-25 16:40:32 +0200

    fix: add revisionId and extra config json field
    The config json field we will configure the action

--- EMS/core-bundle/src/Form/DataField/ActionFieldType.php
+++ EMS/core-bundle/src/Form/DataField/ActionFieldType.php
@@ -5,9 +5,13 @@ declare(strict_types=1);
 namespace EMS\CoreBundle\Form\DataField;
 
 use EMS\CoreBundle\Entity\DataField;
+use EMS\CoreBundle\Entity\Revision;
+use EMS\CoreBundle\Form\Field\IconPickerType;
+use EMS\Helpers\Standard\Json;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Form\FormView;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ActionFieldType extends DataFieldType
 {
@@ -19,11 +23,21 @@ class ActionFieldType extends DataFieldType
 
         $optionsForm
             ->remove('mappingOptions')
-            ->remove('migrationOptions')
-            ->remove('extraOptions');
+            ->remove('migrationOptions');
+
+        $displayOptions = $optionsForm->get('displayOptions');
+        $displayOptions->add('icon', IconPickerType::class, ['required' => false]);
 
         $restrictionOptions = $optionsForm->get('restrictionOptions');
         $restrictionOptions->remove('mandatory')->remove('mandatory_if');
+    }
+
+    #[\Override]
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        parent::configureOptions($resolver);
+
+        $resolver->setDefaults(['icon' => null]);
     }
 
     #[\Override]
@@ -33,8 +47,24 @@ class ActionFieldType extends DataFieldType
 
         /** @var DataField $dataField */
         $dataField = $view->vars['data'];
+        /** @var Revision $revision */
+        $revision = $form->getRoot()->getData();
 
         $view->vars['fieldId'] = $dataField->giveFieldType()->getId();
+        $view->vars['revisionId'] = $revision->getId();
+    }
+
+    public function getDefaultOptions(string $name): array
+    {
+        $defaultOptions = parent::getDefaultOptions($name);
+
+        $defaultOptions['extraOptions']['config'] = Json::encode([
+            'type' => null,
+            'input' => [],
+            'output' => [],
+        ], true);
+
+        return $defaultOptions;
     }
 
     #[\Override]

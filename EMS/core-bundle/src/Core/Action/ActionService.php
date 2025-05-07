79d0616135c009715b52704edc5175a9d35a1b16
David mattei <d.mattei89@gmail.com>
2025-05-07 21:34:46 +0200

    refactor: Action entity to simple CacheAction entity

--- EMS/core-bundle/src/Core/Action/ActionService.php
+++ EMS/core-bundle/src/Core/Action/ActionService.php
@@ -4,26 +4,29 @@ declare(strict_types=1);
 
 namespace EMS\CoreBundle\Core\Action;
 
-use EMS\CoreBundle\Core\Mercure\MercureService;
 use EMS\CoreBundle\Core\Messenger\Message\ActionMessage;
 use EMS\CoreBundle\Core\User\UserManager;
-use EMS\CoreBundle\Entity\Action;
+use EMS\CoreBundle\Entity\CacheAction;
 use EMS\CoreBundle\Entity\Revision;
-use EMS\CoreBundle\Repository\ActionRepository;
+use EMS\CoreBundle\Repository\CacheActionRepository;
 use Ramsey\Uuid\UuidInterface;
 use Symfony\Component\Messenger\MessageBusInterface;
 
 class ActionService
 {
     public function __construct(
-        private readonly ActionRepository $repository,
+        private readonly CacheActionRepository $repository,
         private readonly UserManager $userManager,
-        private readonly MessageBusInterface $bus,
-        private readonly MercureService $mercureService,
+        private readonly MessageBusInterface $bus
     ) {
     }
 
-    public function getById(UuidInterface $id): Action
+    public function cacheResponse(CacheAction $cache): void
+    {
+        $this->repository->save($cache);
+    }
+
+    public function getById(UuidInterface $id): CacheAction
     {
         if (null === $action = $this->repository->findOneBy(['id' => $id])) {
             throw new \RuntimeException(\sprintf('Action with the id %s not found.', $id));
@@ -32,52 +35,20 @@ class ActionService
         return $action;
     }
 
+    public function getCacheResponse(string $requestHash): ?CacheAction
+    {
+        return $this->repository->findOneBy(['requestHash' => $requestHash]);
+    }
+
     /** @param array<mixed> $request */
-    public function requestFromRevision(Revision $revision, array $request): Action
+    public function requestFromRevision(Revision $revision, array $request): void
     {
         $user = $this->userManager->getAuthenticatedUser();
 
-        $action = new Action(
-            sender: 'revision',
-            senderId: (string) $revision->getId(),
-            createdBy: $user->getUsername(),
-            request: $request
-        );
-        $this->repository->save($action);
-
-        $this->bus->dispatch(new ActionMessage($action->getId()));
-
-        return $action;
-    }
-
-    /**
-     * @param array<mixed> $response
-     */
-    public function statusDone(Action $action, array $response): void
-    {
-        $this->repository->save($action->setStatus(ActionStatus::DONE)->setResponse($response));
-        $this->publish($action);
-    }
-
-    public function statusFailed(Action $action, \Throwable $e): void
-    {
-        $this->repository->save(
-            $action
-            ->setStatus(ActionStatus::FAILED)
-            ->setResponse(['error' => $e->getMessage(), 'trace' => $e->getTraceAsString()])
-        );
-    }
-
-    public function statusInProgress(Action $action): void
-    {
-        $this->repository->save($action->setStatus(ActionStatus::IN_PROGRESS));
-    }
-
-    private function publish(Action $action): void
-    {
-        $this->mercureService->publishForUser(
-            data: ['type' => 'action', 'response' => $action->getResponse()],
-            user: $action->getCreatedBy()
-        );
+        $this->bus->dispatch(new ActionMessage(
+            revisionId: $revision->getId(),
+            request: $request,
+            createdBy: $user->getUsername()
+        ));
     }
 }

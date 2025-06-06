73cc76c3aaa6fc425dbcca9346eea3cb6cb68130
Mathieu De Keyzer <me@theus.be>
2025-06-06 13:33:35 +0200

    fix: hide in the menu if turned off

--- EMS/core-bundle/src/Controller/TwigElementsController.php
+++ EMS/core-bundle/src/Controller/TwigElementsController.php
@@ -29,7 +29,8 @@ class TwigElementsController extends AbstractController
         private readonly JobService $jobService,
         private readonly DashboardManager $dashboardManager,
         private readonly ContentTypeService $contentTypeService,
-        private readonly string $templateNamespace
+        private readonly string $templateNamespace,
+        private readonly bool $groupFeature,
     ) {
     }
 
@@ -102,6 +103,9 @@ class TwigElementsController extends AbstractController
             return $menu;
         }
         $menu->addChild(t('key.users', [], 'emsco-core'), 'fa fa-users', Routes::USER_INDEX);
+        if (!$this->groupFeature) {
+            return $menu;
+        }
         $menu->addChild(t('key.groups', [], 'emsco-core'), 'fa fa-list-ul', Routes::GROUP_INDEX);
 
         return $menu;

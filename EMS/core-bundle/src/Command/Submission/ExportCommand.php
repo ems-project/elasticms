d13f6b27d8ed4869a870a9bd9501b2480795b1eb
Jerome Coppee <j.coppee@gmail.com>
2025-06-12 18:38:09 +0200

    refactor(Core/ExportCommand): move logic in service

--- EMS/core-bundle/src/Command/Submission/ExportCommand.php
+++ EMS/core-bundle/src/Command/Submission/ExportCommand.php
@@ -6,17 +6,11 @@ namespace EMS\CoreBundle\Command\Submission;
 
 use EMS\CommonBundle\Common\Admin\AdminHelper;
 use EMS\CommonBundle\Common\Command\AbstractCommand;
-use EMS\CommonBundle\Common\PropertyAccess\PropertyAccessor;
-use EMS\CommonBundle\Contracts\Spreadsheet\SpreadsheetGeneratorServiceInterface;
-use EMS\CommonBundle\Service\ExpressionService;
 use EMS\CommonBundle\Storage\File\FileInterface;
 use EMS\CommonBundle\Storage\NotFoundException;
 use EMS\CommonBundle\Storage\StorageManager;
 use EMS\CoreBundle\Commands;
-use EMS\CoreBundle\Core\Mail\MailerService;
-use EMS\CoreBundle\Service\Form\Submission\FormSubmissionService;
-use EMS\Helpers\File\File;
-use EMS\Helpers\File\TempFile;
+use EMS\CoreBundle\Service\Submission\SubmissionExporter;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
@@ -33,26 +27,11 @@ class ExportCommand extends AbstractCommand
     public const string MAIL_TEMPLATE = '@EMSCore/email/submissions-export.html.twig';
     public const string ARGUMENT_CONFIG_FILE = 'config-file';
     private ExportConfig $exportConfig;
-    private string $configFilename;
-    /** @var mixed[] */
-    private array $columns;
-    private ?string $filter = null;
-    private ?string $filename = null;
-    /**
-     * @var string[]
-     */
-    private array $emailsTo;
-    private string $subject;
-    private ?string $format = null;
 
     public function __construct(
-        private readonly FormSubmissionService $formSubmissionService,
-        private readonly ExpressionService $expressionService,
-        private readonly SpreadsheetGeneratorServiceInterface $spreadsheetGeneratorService,
-        private readonly MailerService $mailerService,
+        private readonly SubmissionExporter $exporter,
         private readonly StorageManager $storageManager,
         private readonly AdminHelper $adminHelper,
-        private readonly Environment $templating,
     ) {
         parent::__construct();
     }
@@ -60,7 +39,7 @@ class ExportCommand extends AbstractCommand
     #[\Override]
     protected function configure(): void
     {
-        $this->addArgument(self::ARGUMENT_CONFIG_FILE, InputArgument::REQUIRED, 'JSON config file (filename)');
+        $this->addArgument(self::ARGUMENT_CONFIG_FILE, InputArgument::REQUIRED, 'JSON config file (path)');
     }
 
     #[\Override]
@@ -68,121 +47,18 @@ class ExportCommand extends AbstractCommand
     {
         parent::initialize($input, $output);
 
-        $this->configFilename = $this->getArgumentString(self::ARGUMENT_CONFIG_FILE);
-        $this->exportConfig = ExportConfig::fromJson($this->getFile($this->configFilename)->getContent());
+        $fileIdentifier = $this->getArgumentString(self::ARGUMENT_CONFIG_FILE);
+        $file = $this->getFile($fileIdentifier);
 
-        $this->columns = $this->exportConfig->columns;
-        $this->filter = $this->exportConfig->filter;
-        $this->filename = $this->exportConfig->filename ?? null;
-        $this->emailsTo = $this->exportConfig->emailsTo;
-        $this->subject = $this->exportConfig->subject;
-        $this->format = $this->exportConfig->format;
+        $this->exportConfig = ExportConfig::fromJson($file->getContent());
     }
 
     #[\Override]
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $this->io->section('Export the form submissions');
-        $sheet = [];
-
-        $this->io->progressStart($this->formSubmissionService->count());
-        $propertyAccessor = PropertyAccessor::createPropertyAccessor();
-        foreach ($this->formSubmissionService->getUnprocessed() as $submission) {
-            $data = [
-                'instance' => $submission->getInstance(),
-                'name' => $submission->getName(),
-                'locale' => $submission->getLocale(),
-                'submission_date' => $submission->getCreated()->format('c'),
-                'data' => $submission->getData() ?? [],
-            ];
-            if (null !== $this->filter && !$this->expressionService->evaluateToBool($this->filter, $data)) {
-                $this->io->progressAdvance();
-                continue;
-            }
-            $line = [];
-            foreach ($this->columns as $column) {
-                if (!empty($column['field'])) {
-                    $line[] = $propertyAccessor->getValue($data, $column['field']) ?? '';
-                } elseif (!empty($column['template'])) {
-                    $template = $this->templating->load($column['template']);
-                    if (!empty($column['block'])) {
-                        $field = $template->renderBlock($column['block'], \compact('data'));
-                    } else {
-                        $field = $template->render(\compact('data'));
-                    }
-                    $line[] = $field;
-                }
-            }
-            $sheet[] = $line;
-            $this->io->progressAdvance();
-        }
-        $this->io->progressFinish();
-        $headers = \array_column($this->columns, 'name');
-        if (null === $this->filename && empty($this->emailsTo)) {
-            $this->io->table([...$headers], $sheet);
-
-            return self::EXECUTE_SUCCESS;
-        }
-        $extension = $this->getFormat();
-
-        $config = [
-            SpreadsheetGeneratorServiceInterface::SHEETS => [[
-                'rows' => [[...$headers], ...$sheet],
-                'name' => 'submissions',
-            ]],
-            SpreadsheetGeneratorServiceInterface::CONTENT_FILENAME => 'submissions',
-            SpreadsheetGeneratorServiceInterface::WRITER => $extension,
-        ];
-        $tempFile = TempFile::create();
-        $this->spreadsheetGeneratorService->generateSpreadsheetFile($config, $tempFile->path);
-
-        $this->generateFile($tempFile);
-        $this->sendEmail($tempFile);
-        $this->io->success('Export '.\count($sheet).' submission(s) done !');
+        $this->exporter->export($this->exportConfig, $this->io);
 
         return self::EXECUTE_SUCCESS;
-    }
-
-    private function generateFile(TempFile $tempFile): void
-    {
-        if (null == $this->filename) {
-            return;
-        }
-        File::putContents($this->filename, $tempFile->getContents());
-        $this->io->success(\sprintf('The file %s has been successfully generated', $this->filename));
-    }
-
-    private function sendEmail(TempFile $tempFile): void
-    {
-        if (empty($this->emailsTo)) {
-            return;
-        }
-        $mailTemplate = $this->mailerService->makeMailTemplate(self::MAIL_TEMPLATE);
-        foreach ($this->emailsTo as $email) {
-            $mailTemplate->addTo($email);
-        }
-        $mailTemplate->setSubject($this->subject);
-        $mailTemplate->setBodyBlock('body');
-        $mailTemplate->addAttachment($tempFile->path, \sprintf('crm-export.%s', $this->format));
-        $this->mailerService->sendMailTemplate($mailTemplate);
-    }
-
-    private function getFormat(): string
-    {
-        $fileExtension = null;
-        if (null !== $this->filename) {
-            $fileExtension = \pathinfo($this->filename)['extension'] ?? null;
-            if (!\in_array($fileExtension, SpreadsheetGeneratorServiceInterface::FORMAT_WRITERS, true)) {
-                $this->io->error(\sprintf('File extension %s is not supported', $fileExtension));
-            }
-        }
-        $this->format ??= $fileExtension ?? SpreadsheetGeneratorServiceInterface::XLSX_WRITER;
-
-        if (null !== $fileExtension && $fileExtension !== $this->format) {
-            $this->io->error(\sprintf('Export format %s mismatched with the file extension %s', $this->format, $this->filename));
-        }
-
-        return $this->format;
     }
 
     private function getFile(string $fileIdentifier): FileInterface
---
+++ EMS/core-bundle/src/Service/Submission/SubmissionExporter.php
@@ -0,0 +1,142 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EMS\CoreBundle\Service\Submission;
+
+use EMS\CommonBundle\Contracts\Spreadsheet\SpreadsheetGeneratorServiceInterface;
+use EMS\CommonBundle\Service\ExpressionService;
+use EMS\CoreBundle\Command\Submission\ExportCommand;
+use EMS\CoreBundle\Command\Submission\ExportConfig;
+use EMS\CoreBundle\Core\Mail\MailerService;
+use EMS\CoreBundle\Service\Form\Submission\FormSubmissionService;
+use EMS\Helpers\File\File;
+use EMS\Helpers\File\TempFile;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\PropertyAccess\PropertyAccessorInterface;
+use Twig\Environment;
+
+final readonly class SubmissionExporter
+{
+    public function __construct(
+        private FormSubmissionService $formSubmissionService,
+        private ExpressionService $expressionService,
+        private SpreadsheetGeneratorServiceInterface $spreadsheetGeneratorService,
+        private MailerService $mailerService,
+        private Environment $templating,
+        private PropertyAccessorInterface $propertyAccessor,
+    ) {}
+
+    public function export(ExportConfig $config, SymfonyStyle $io): void
+    {
+        $io->section('Exporting form submissions');
+        $sheet = [];
+
+        $io->progressStart($this->formSubmissionService->count());
+
+        foreach ($this->formSubmissionService->getUnprocessed() as $submission) {
+            $data = [
+                'instance' => $submission->getInstance(),
+                'name' => $submission->getName(),
+                'locale' => $submission->getLocale(),
+                'submission_date' => $submission->getCreated()->format('c'),
+                'data' => $submission->getData() ?? [],
+            ];
+
+            if ($config->filter && !$this->expressionService->evaluateToBool($config->filter, $data)) {
+                $io->progressAdvance();
+                continue;
+            }
+
+            $line = [];
+            foreach ($config->columns as $column) {
+                $line[] = $this->renderColumn($column, $data);
+            }
+
+            $sheet[] = $line;
+            $io->progressAdvance();
+        }
+
+        $io->progressFinish();
+        $headers = array_column($config->columns, 'name');
+
+        if (null === $config->filename && empty($config->emailsTo)) {
+            $io->table($headers, $sheet);
+            return;
+        }
+
+        $extension = $this->determineFormat($config, $io);
+        $tempFile = TempFile::create();
+
+        $this->spreadsheetGeneratorService->generateSpreadsheetFile([
+            SpreadsheetGeneratorServiceInterface::SHEETS => [[
+                'rows' => [[...$headers], ...$sheet],
+                'name' => 'submissions',
+            ]],
+            SpreadsheetGeneratorServiceInterface::CONTENT_FILENAME => 'submissions',
+            SpreadsheetGeneratorServiceInterface::WRITER => $extension,
+        ], $tempFile->path);
+
+        if ($config->filename) {
+            File::putContents($config->filename, $tempFile->getContents());
+            $io->success(sprintf('File %s generated', $config->filename));
+        }
+
+        if (!empty($config->emailsTo)) {
+            $this->sendEmail($tempFile, $config);
+            $io->success('Email(s) sent');
+        }
+
+        $io->success(sprintf('Exported %d submissions', count($sheet)));
+    }
+
+    private function renderColumn(array $column, array $data): string
+    {
+        
+        if (!empty($column['field'])) {
+            return $this->propertyAccessor->getValue($data, $column['field']) ?? '';
+        }
+
+        if (!empty($column['template'])) {
+            $template = $this->templating->load($column['template']);
+            return !empty($column['block'])
+                ? $template->renderBlock($column['block'], compact('data'))
+                : $template->render(compact('data'));
+        }
+
+        return '';
+    }
+
+    private function determineFormat(ExportConfig $config, SymfonyStyle $io): string
+    {
+        $fileExtension = $config->filename ? pathinfo($config->filename, PATHINFO_EXTENSION) : null;
+
+        if ($fileExtension && !in_array($fileExtension, SpreadsheetGeneratorServiceInterface::FORMAT_WRITERS, true)) {
+            throw new \InvalidArgumentException("Unsupported file extension: $fileExtension");
+        }
+
+        $format = $config->format ?? $fileExtension ?? SpreadsheetGeneratorServiceInterface::XLSX_WRITER;
+
+        if ($fileExtension && $format !== $fileExtension) {
+            $io->warning(sprintf('Export format %s mismatched with file extension %s', $format, $fileExtension));
+        }
+
+        return $format;
+    }
+
+    private function sendEmail(TempFile $tempFile, ExportConfig $config): void
+    {
+        $mailTemplate = $this->mailerService->makeMailTemplate(ExportCommand::MAIL_TEMPLATE);
+
+        foreach ($config->emailsTo as $email) {
+            $mailTemplate->addTo($email);
+        }
+
+        $mailTemplate
+            ->setSubject($config->subject)
+            ->setBodyBlock('body')
+            ->addAttachment($tempFile->path, sprintf('crm-export.%s', $config->format));
+
+        $this->mailerService->sendMailTemplate($mailTemplate);
+    }
+}

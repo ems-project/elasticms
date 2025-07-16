a9db84c3acbe3b134eff930d754a047c18f3326d
David mattei <d.mattei89@gmail.com>
2025-07-16 07:02:54 +0200

    fix: better way for using translations in choices

--- EMS/core-bundle/src/Form/Form/WysiwygProfileType.php
+++ EMS/core-bundle/src/Form/Form/WysiwygProfileType.php
@@ -35,11 +35,11 @@ class WysiwygProfileType extends AbstractType
             ->add('editor', ChoiceType::class, [
                 'required' => true,
                 'choices' => [
-                    'wysiwyg_profile_editor.ckeditor4' => WysiwygProfile::CKEDITOR4,
-                    'wysiwyg_profile_editor.ckeditor5' => WysiwygProfile::CKEDITOR5,
+                    t('key.ckeditor4', [], 'emsco-core')->getMessage() => WysiwygProfile::CKEDITOR4,
+                    t('key.ckeditor5', [], 'emsco-core')->getMessage() => WysiwygProfile::CKEDITOR5,
                 ],
                 'label' => t('field.editor', [], 'emsco-core'),
-                'choice_translation_domain' => 'emsco-choices',
+                'choice_translation_domain' => 'emsco-core',
             ])
             ->add('config', CodeEditorType::class, [
                 'language' => 'ace/mode/json',

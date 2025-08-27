ae887b6d45447c97731767b1c96aaea3a4b6d8ce
David mattei <d.mattei89@gmail.com>
2025-08-27 09:21:46 +0200

    fix: mixed on data cell

--- EMS/common-bundle/src/Common/Spreadsheet/SpreadsheetGeneratorService.php
+++ EMS/common-bundle/src/Common/Spreadsheet/SpreadsheetGeneratorService.php
@@ -190,7 +190,7 @@ final class SpreadsheetGeneratorService implements SpreadsheetGeneratorServiceIn
 
         return $resolved;
     }
-    
+
     private function buildCellFromValue(mixed $config): Cell
     {
         $config = \is_array($config) ? $config : [Cell::CELL_DATA => $config];
@@ -210,7 +210,7 @@ final class SpreadsheetGeneratorService implements SpreadsheetGeneratorServiceIn
             ->setAllowedTypes(Cell::CELL_FORMAT_DISPLAY, ['null', 'string'])
         ;
 
-        /** @var array{data: string, type?: string, style: array<mixed>, format_input?: string, format_display?: string} $resolved */
+        /** @var array{data: mixed, type?: string, style: array<mixed>, format_input?: string, format_display?: string} $resolved */
         $resolved = $resolver->resolve($config);
 
         return new Cell(

e301ecf0889b92d3d8416f5bc7caea472ecab666
Mathieu De Keyzer <me@theus.be>
2025-09-08 09:27:13 +0200

    feat: ArrayHelper::arrays_are_equal_recursive

--- EMS/helpers/src/ArrayHelper/ArrayHelper.php
+++ EMS/helpers/src/ArrayHelper/ArrayHelper.php
@@ -8,4 +8,31 @@ class ArrayHelper
 {
     use FindHelperTrait;
     use MapHelperTrait;
+
+    /**
+     * @param mixed|mixed[] $array1
+     * @param mixed|mixed[] $array2
+     * @return bool
+     */
+    public static function arrays_are_equal_recursive($array1, $array2): bool
+    {
+        if (!\is_array($array1) || !\is_array($array2)) {
+            return $array1 === $array2;
+        }
+
+        if (\count($array1) !== \count($array2)) {
+            return false;
+        }
+
+        foreach ($array1 as $key => $value) {
+            if (!\array_key_exists($key, $array2)) {
+                return false;
+            }
+            if (!self::arrays_are_equal_recursive($value, $array2[$key])) {
+                return false;
+            }
+        }
+
+        return true;
+    }
 }

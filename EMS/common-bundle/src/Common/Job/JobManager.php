4c7125862f8c3a3c87e362d0afeb606e55040956
David mattei <d.mattei89@gmail.com>
2025-10-17 15:27:50 +0200

    fix(common/job): only success code should mark the job as done

--- EMS/common-bundle/src/Common/Job/JobManager.php
+++ EMS/common-bundle/src/Common/Job/JobManager.php
@@ -7,6 +7,7 @@ namespace EMS\CommonBundle\Common\Job;
 use EMS\CommonBundle\Common\Admin\AdminHelper;
 use EMS\CommonBundle\Common\CoreApi\Endpoint\Admin\Message\Job;
 use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\StringInput;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\HttpKernel\KernelInterface;
@@ -31,8 +32,13 @@ class JobManager
             $escapedCommand = u($command)->replace('\\', '\\\\')->toString();
             $input = new StringInput($escapedCommand);
 
-            $application->run($input, $output);
-            $this->adminHelper->getCoreApi()->admin()->jobCompleted($job);
+            $result = $application->run($input, $output);
+
+            if (Command::SUCCESS === $result) {
+                $this->adminHelper->getCoreApi()->admin()->jobCompleted($job);
+            } else {
+                $this->adminHelper->getCoreApi()->admin()->jobFailed($job, \sprintf('return code %d', $result));
+            }
         } catch (\Exception $e) {
             $this->adminHelper->getCoreApi()->admin()->jobFailed($job, $e->getMessage());
             $output->writeln(\sprintf('An exception has been raised: %s', $e->getMessage()));

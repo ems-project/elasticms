46622b3e8b14a0576c1fe3e64764b7682dc4b9c7
Mathieu De Keyzer <me@theus.be>
2025-10-27 21:11:26 +0100

    fix: phpstan

--- EMS/common-bundle/src/Helper/MimeTypeHelper.php
+++ EMS/common-bundle/src/Helper/MimeTypeHelper.php
@@ -34,7 +34,7 @@ class MimeTypeHelper
         } else {
             $mimeType = null;
         }
-        if (null === $mimeType || \str_starts_with($mimeType ?? '', 'text/')) {
+        if (null === $mimeType || \str_starts_with($mimeType, 'text/')) {
             $ext = \pathinfo($filename, PATHINFO_EXTENSION);
             $mimeType = $this->mimeTypes->getMimeTypes($ext)[0] ?? $mimeType;
         }

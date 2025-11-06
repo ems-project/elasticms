85e33dfc81bebe56f4a869e7533d26b99543373d
David mattei <d.mattei89@gmail.com>
2025-11-06 14:12:16 +0100

    phpcs & phpstan

--- EMS/core-bundle/src/Core/Component/MediaLibrary/Config/MediaLibraryConfigSort.php
+++ EMS/core-bundle/src/Core/Component/MediaLibrary/Config/MediaLibraryConfigSort.php
@@ -7,15 +7,15 @@ namespace EMS\CoreBundle\Core\Component\MediaLibrary\Config;
 class MediaLibraryConfigSort
 {
     public function __construct(
-        public readonly string  $id,
-        public readonly string  $field,
+        public readonly string $id,
+        public readonly string $field,
         public readonly ?string $defaultOrder,
         public readonly ?string $parentField,
     ) {
     }
 
     /**
-     * @return array<string, array{order: string, nested?: array{path: string}>
+     * @return array<string, array{order: string, nested?: array{path: string}}>
      */
     public function getQuery(string $order): array
     {

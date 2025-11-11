06edb8405c928e43264b5adf94b11455649ab4d2
Davidmattei <d.mattei89@gmail.com>
2025-11-11 21:21:36 +0100

    fix: remove commented navbar

--- docs/.vitepress/nav/navbar.ts
+++ docs/.vitepress/nav/navbar.ts
@@ -2,20 +2,7 @@ import type {DefaultTheme} from 'vitepress'
 
 const navbar: DefaultTheme.NavItem[] = [
     { text: 'Home', link: '/' },
-    { text: 'Upgrade', link: '/upgrade' },
-    // {
-    //     text: 'Guide',
-    //     items: [
-    //         {text: 'Getting Started', link: '/guide/getting-started'},
-    //     ]
-    // },
-    // {
-    //     text: 'Releases',
-    //     items: [
-    //         {text: 'Upgrade 5.x', link: '/release/upgrade-5x'},
-    //         {text: 'Notes 5.x', link: 'https://github.com/ems-project/elasticms/blob/5.x/CHANGELOG-5.x.md'},
-    //     ]
-    // },
+    { text: 'Upgrade', link: '/upgrade' }
 ]
 
 export default navbar;

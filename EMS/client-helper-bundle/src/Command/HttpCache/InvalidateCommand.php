00fdc7887204eb42a513ae0babb9e3c180a113c3
Mathieu De Keyzer <me@theus.be>
2025-11-13 17:56:04 +0100

    feat: get last published date from default environment

--- EMS/client-helper-bundle/src/Command/HttpCache/InvalidateCommand.php
+++ EMS/client-helper-bundle/src/Command/HttpCache/InvalidateCommand.php
@@ -5,8 +5,12 @@ declare(strict_types=1);
 namespace EMS\ClientHelperBundle\Command\HttpCache;
 
 use EMS\ClientHelperBundle\Commands;
+use EMS\ClientHelperBundle\Helper\Elasticsearch\ClientRequestManager;
 use EMS\CommonBundle\Common\Cache\Cache;
 use EMS\CommonBundle\Common\Command\AbstractCommand;
+use EMS\CommonBundle\Helper\EmsFields;
+use EMS\Helpers\Standard\DateTime;
+use EMS\Helpers\Standard\Type;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
@@ -23,7 +27,7 @@ class InvalidateCommand extends AbstractCommand
     private const string OPTION_PURGE = 'purge';
     private bool $purge;
 
-    public function __construct(private readonly Cache $cacheManager)
+    public function __construct(private readonly Cache $cacheManager, private readonly ClientRequestManager $manager)
     {
         parent::__construct();
     }
@@ -45,19 +49,43 @@ class InvalidateCommand extends AbstractCommand
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $cache = $this->cacheManager->getItem(self::LAST_HTTP_CACHE_INVALIDATE_DATETIME);
-        $dateTime = new \DateTimeImmutable();
         if ($this->purge || !$cache->isHit()) {
-            $this->io->title('Purge all caches');
+            $lastPublishedDate = $this->purge();
         } else {
             $lastHttpCacheInvalidate = $cache->get();
-            if (!$lastHttpCacheInvalidate instanceof \DateTimeInterface) {
+            if (!$lastHttpCacheInvalidate instanceof \DateTimeImmutable) {
                 throw new \LogicException('Last http cache invalidate date time is not valid.');
             }
-            $this->io->title(\sprintf('Invalidate HTTP caches from %s', $lastHttpCacheInvalidate->format('Y-m-d H:i:s')));
+            $lastPublishedDate = $this->ban($lastHttpCacheInvalidate);
         }
-        $cache->set($dateTime);
+        $cache->set($lastPublishedDate);
         $this->cacheManager->save($cache);
 
         return self::SUCCESS;
+    }
+
+    private function purge(): \DateTimeImmutable
+    {
+        $this->io->title('Purge all caches');
+
+        $client = $this->manager->getDefault();
+        $search = $client->initializeCommonSearch([]);
+        $search->setSort([EmsFields::CONTENT_PUBLISHED_DATETIME_FIELD => 'desc']);
+        $search->setSize(1);
+        $search->setSources([EmsFields::CONTENT_PUBLISHED_DATETIME_FIELD]);
+        $results = $client->commonSearch($search)->getResults();
+        if (!isset($results[0])) {
+            throw new \RuntimeException('No results found');
+        }
+        $lastPublishedDate = $results[0]->getSource()[EmsFields::CONTENT_PUBLISHED_DATETIME_FIELD] ?? null;
+
+        return DateTime::create(Type::string($lastPublishedDate));
+    }
+
+    private function ban(\DateTimeImmutable $from): \DateTimeImmutable
+    {
+        $this->io->title(\sprintf('Invalidate HTTP caches from %s', $from->format('Y-m-d H:i:s')));
+
+        return $from;
     }
 }

6d12ba884827f38ac3fed0d587d0a4826bbdf345
Mathieu De Keyzer <me@theus.be>
2025-11-13 17:26:22 +0100

    feat: purge option

--- EMS/client-helper-bundle/src/Command/HttpCache/InvalidateCommand.php
+++ EMS/client-helper-bundle/src/Command/HttpCache/InvalidateCommand.php
@@ -9,6 +9,7 @@ use EMS\CommonBundle\Common\Cache\Cache;
 use EMS\CommonBundle\Common\Command\AbstractCommand;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 #[AsCommand(
@@ -19,6 +20,8 @@ use Symfony\Component\Console\Output\OutputInterface;
 class InvalidateCommand extends AbstractCommand
 {
     private const LAST_HTTP_CACHE_INVALIDATE_DATETIME = 'last-http-cache-invalidate-date-time';
+    private const string OPTION_PURGE = 'purge';
+    private bool $purge;
 
     public function __construct(private readonly Cache $cacheManager)
     {
@@ -26,18 +29,31 @@ class InvalidateCommand extends AbstractCommand
     }
 
     #[\Override]
+    protected function configure(): void
+    {
+        $this->addOption(self::OPTION_PURGE, 'p', InputOption::VALUE_NONE, 'Purge all caches.');
+    }
+
+    #[\Override]
+    protected function initialize(InputInterface $input, OutputInterface $output): void
+    {
+        parent::initialize($input, $output);
+        $this->purge = $this->getOptionBool(self::OPTION_PURGE);
+    }
+
+    #[\Override]
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $cache = $this->cacheManager->getItem(self::LAST_HTTP_CACHE_INVALIDATE_DATETIME);
         $dateTime = new \DateTimeImmutable();
-        if ($cache->isHit()) {
+        if ($this->purge || !$cache->isHit()) {
+            $this->io->title('Purge all caches');
+        } else {
             $lastHttpCacheInvalidate = $cache->get();
             if (!$lastHttpCacheInvalidate instanceof \DateTimeInterface) {
                 throw new \LogicException('Last http cache invalidate date time is not valid.');
             }
             $this->io->title(\sprintf('Invalidate HTTP caches from %s', $lastHttpCacheInvalidate->format('Y-m-d H:i:s')));
-        } else {
-            $this->io->title('Purge all caches');
         }
         $cache->set($dateTime);
         $this->cacheManager->save($cache);

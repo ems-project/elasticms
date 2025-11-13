8b265befbd262ae236e7622496253d04eb83c093
Mathieu De Keyzer <me@theus.be>
2025-11-13 17:21:06 +0100

    feat: commands that get the last invalidate datetime

---
+++ EMS/client-helper-bundle/src/Command/HttpCache/InvalidateCommand.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EMS\ClientHelperBundle\Command\HttpCache;
+
+use EMS\ClientHelperBundle\Commands;
+use EMS\CommonBundle\Common\Cache\Cache;
+use EMS\CommonBundle\Common\Command\AbstractCommand;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+#[AsCommand(
+    name: Commands::HTTP_CACHE_INVALIDATE,
+    description: 'Clear http cache in reverse proxies (e.g. varnish)',
+    hidden: false
+)]
+class InvalidateCommand extends AbstractCommand
+{
+    private const LAST_HTTP_CACHE_INVALIDATE_DATETIME = 'last-http-cache-invalidate-date-time';
+
+    public function __construct(private readonly Cache $cacheManager)
+    {
+        parent::__construct();
+    }
+
+    #[\Override]
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $cache = $this->cacheManager->getItem(self::LAST_HTTP_CACHE_INVALIDATE_DATETIME);
+        $dateTime = new \DateTimeImmutable();
+        if ($cache->isHit()) {
+            $lastHttpCacheInvalidate = $cache->get();
+            if (!$lastHttpCacheInvalidate instanceof \DateTimeInterface) {
+                throw new \LogicException('Last http cache invalidate date time is not valid.');
+            }
+            $this->io->title(\sprintf('Invalidate HTTP caches from %s', $lastHttpCacheInvalidate->format('Y-m-d H:i:s')));
+        } else {
+            $this->io->title('Purge all caches');
+        }
+        $cache->set($dateTime);
+        $this->cacheManager->save($cache);
+
+        return self::SUCCESS;
+    }
+}

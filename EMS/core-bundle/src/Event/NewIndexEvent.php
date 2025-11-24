36faae800b90c9ccb8c52b2debd67d366e775c67
Mathieu De Keyzer <me@theus.be>
2025-11-24 17:15:21 +0100

    feat: NewIndexEvent

---
+++ EMS/core-bundle/src/Event/NewIndexEvent.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EMS\CoreBundle\Event;
+
+use Symfony\Contracts\EventDispatcher\Event;
+
+/**
+ * @author Mathieu De Keyzer <ems@theus.be>
+ */
+class NewIndexEvent extends Event
+{
+    final public const string NAME = 'ems_core.environment.new_index';
+
+    /**
+     * @param string[] $aliases
+     */
+    public function __construct(private readonly string $index, private readonly array $aliases, private readonly ?string $oldIndex)
+    {
+    }
+
+    public function getIndex(): string
+    {
+        return $this->index;
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getAliases(): array
+    {
+        return $this->aliases;
+    }
+
+    public function getOldIndex(): ?string
+    {
+        return $this->oldIndex;
+    }
+}

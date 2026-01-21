98f472c052d6f0a618bdc6434564b18573db72f6
Mathieu De Keyzer <me@theus.be>
2026-01-21 18:16:10 +0100

    feat: options

--- EMS/xliff/src/Options.php
+++ EMS/xliff/src/Options.php
@@ -4,6 +4,14 @@ declare(strict_types=1);
 
 namespace EMS\Xliff;
 
-class Options
+use EMS\Xliff\Html\Policy\TranslatableAttributes;
+
+final readonly class Options
 {
+    public function __construct(
+        public string $defaultVersion = Version::V12,
+        public bool $preserveWhitespace = true,
+        public ?TranslatableAttributes $translatableAttributes = null,
+    ) {
+    }
 }

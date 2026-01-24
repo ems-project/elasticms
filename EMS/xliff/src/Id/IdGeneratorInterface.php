da0e705729117059d1b70e64235e84e80a96c07f
Mathieu De Keyzer <me@theus.be>
2026-01-24 08:06:25 +0100

    feat: IdGenerator

---
+++ EMS/xliff/src/Id/IdGeneratorInterface.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EMS\Xliff\Id;
+
+interface IdGeneratorInterface
+{
+    public function nextUnitId(): string;
+
+    public function nextSegmentId(): string;
+
+    public function nextGroupId(): string;
+
+    public function nextInlineCodeId(): string;
+
+    public function nextEndInlineCodeId(): string;
+
+    public function nextPlaceholderId(): string;
+}

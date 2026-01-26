dc304f6dbc8c82b0f7cfb6b3fdad16adef8ee8b5
David mattei <d.mattei89@gmail.com>
2026-01-26 13:24:16 +0100

    phpcs

--- EMS/core-bundle/src/Command/Revision/TimeMachineCommand.php
+++ EMS/core-bundle/src/Command/Revision/TimeMachineCommand.php
@@ -137,8 +137,9 @@ final class TimeMachineCommand extends Command
     {
         $property = \array_shift($path);
 
-        if (!is_string($property)) {
+        if (!\is_string($property)) {
             $this->style->error('Property path is empty');
+
             return $currentRaw;
         }
 

72418ea4e38b71a5e26762e5dc823f73eeb1b858
Sébastian Molle <sebastian.molle@gmail.com>
2026-02-06 18:44:15 +0100

    feat: ElasticMS Admin metrics

--- elasticms-admin/.docker/bin/container-entrypoint.d/elasticms.d/40-nginx.sh
+++ elasticms-admin/.docker/bin/container-entrypoint.d/elasticms.d/40-nginx.sh
@@ -14,4 +14,31 @@ if [[ ! -z ${NGINX_ENABLED} ]] && [[ ${NGINX_ENABLED,,} = true ]]; then
 
     cat ${APP_CONFIG_DIR}/${ELASTICMS_INSTANCE_NAME} | sed '/^\s*$/d' | grep  -v '^#' | sed "s/\([a-zA-Z0-9_]*\)\=\(.*\)/fastcgi_param \1 \2;/g" >> /opt/etc/nginx/conf.d/${ELASTICMS_INSTANCE_NAME}.fastcgi_params
 
+    if [[ ! -z ${EMS_METRIC_ENABLED} ]] && [[ ${EMS_METRIC_ENABLED,,} = true ]]; then
+
+        if [ ! -f /opt/etc/nginx/sites-enabled/__metrics_elasticms.conf ] ; then
+
+            log "INFO" "+ Configure [ ${ELASTICMS_INSTANCE_NAME} metrics ] VirtualHost for ElasticMS WebSite on [ ${METRICS_VHOST_SERVER_NAME} ]."
+
+            gomplate -f /opt/config/nginx/sites-enabled/elasticms-metrics.conf.gtpl \
+                     -o /opt/etc/nginx/sites-enabled/__metrics_elasticms.conf
+
+        fi
+
+    fi
+
+    if [[ ! -z ${METRICS_ENABLED} ]] && [[ ${METRICS_ENABLED,,} = true ]]; then
+
+        if [ ! -f /opt/etc/nginx/sites-enabled/__metrics.conf ] ; then
+
+            log "INFO" "+ Configure [ nginx/fpm metrics ] VirtualHost for ElasticMS WebSite on [ ${METRICS_VHOST_SERVER_NAME} ]."
+
+            gomplate -f /opt/config/nginx/sites-enabled/__metrics.conf.gtpl \
+                     -o /opt/etc/nginx/sites-enabled/__metrics.conf
+
+        fi
+
+    fi
+
 fi
+

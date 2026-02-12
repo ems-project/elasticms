9e3913f3be8996a92678c2f00dba052317619561
David mattei <d.mattei89@gmail.com>
2026-02-12 11:33:01 +0100

    fix: submission bundle attribute extension

--- EMS/submission-bundle/src/Twig/SubmissionExtension.php
+++ EMS/submission-bundle/src/Twig/SubmissionExtension.php
@@ -6,19 +6,23 @@ namespace EMS\SubmissionBundle\Twig;
 
 use EMS\SubmissionBundle\Connection\Transformer;
 use EMS\SubmissionBundle\Exception\SkipSubmissionException;
-use Twig\Extension\RuntimeExtensionInterface;
+use Twig\Attribute\AsTwigFilter;
+use Twig\Attribute\AsTwigFunction;
+use Twig\Extension\AbstractExtension;
 
-final readonly class ConnectionRuntime implements RuntimeExtensionInterface
+class SubmissionExtension extends AbstractExtension
 {
-    public function __construct(private Transformer $transformer)
+    public function __construct(private readonly Transformer $transformer)
     {
     }
 
+    #[AsTwigFilter(name: 'emss_connection', isSafe: ['html'])]
     public function transform(string $content): string
     {
         return $this->transformer->transform(\explode('%.%', $content));
     }
 
+    #[AsTwigFunction(name: 'emss_skip_submit', isSafe: ['html'])]
     public function skipSubmitException(): never
     {
         throw new SkipSubmissionException();

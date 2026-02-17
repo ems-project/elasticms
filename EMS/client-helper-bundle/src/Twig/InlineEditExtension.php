7a3d5728ed662d82ff8f39a0749f8b5d45b2f0a9
David mattei <d.mattei89@gmail.com>
2026-02-17 13:56:49 +0100

    add required attributes (data emsId and path, class inline-edit-element)

--- EMS/client-helper-bundle/src/Twig/InlineEditExtension.php
+++ EMS/client-helper-bundle/src/Twig/InlineEditExtension.php
@@ -17,11 +17,20 @@ class InlineEditExtension
     {
         $content = $document->getValue($path);
 
+        if (isset($attributes['class'])) {
+            $attributes['class'] .= ' inline-edit-element';
+        } else {
+            $attributes['class'] = 'inline-edit-element';
+        }
+
+        $attributes['data-ems-id'] = (string) $document->getEmsLink();
+        $attributes['data-path'] = $path;
+
         $attrString = '';
         foreach ($attributes as $key => $value) {
             $attrString .= \sprintf(' %s="%s"', $key, \htmlspecialchars($value, ENT_QUOTES));
         }
 
-        return \sprintf('<%1$s%2$s>%3$s</%1$s>', $element, $attrString, \htmlspecialchars($content));
+        return \sprintf('<%1$s%2$s>%3$s</%1$s>', $element, $attrString, \htmlspecialchars((string) $content));
     }
 }

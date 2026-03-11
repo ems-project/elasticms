dc74046949303df5e44a151134b6ac0cc81eb50d
David mattei <d.mattei89@gmail.com>
2026-03-11 14:52:44 +0100

    fix: request inline edit already editing

--- EMS/admin-ui-bundle/assets/src/core/inline-editor/iframe/iframe.ts
+++ EMS/admin-ui-bundle/assets/src/core/inline-editor/iframe/iframe.ts
@@ -68,7 +68,7 @@ export class Iframe {
   private onClick(event: MouseEvent): void {
     const target = event.target as HTMLElement
 
-    if (target.matches(this.inlineSelectors.join(','))) {
+    if (target.matches(this.inlineSelectors.join(',')) && target !== this.editElement) {
       const inlineElement = this.getInlineElement(target);
       if (null === inlineElement) return;
 

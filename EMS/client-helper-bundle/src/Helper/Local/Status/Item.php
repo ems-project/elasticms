09e7743993f829d521d423d39a737900e85a21b6
Mathieu De Keyzer <me@theus.be>
2026-03-14 14:43:14 +0100

    chore: phpcs

--- EMS/client-helper-bundle/src/Helper/Local/Status/Item.php
+++ EMS/client-helper-bundle/src/Helper/Local/Status/Item.php
@@ -33,7 +33,7 @@ final class Item
             return false;
         }
 
-        if (!$this->hasDataLocal() || \array_filter($this->dataLocal, fn($v) => $v !== null) === $this->dataOrigin) {
+        if (!$this->hasDataLocal() || \array_filter($this->dataLocal, fn ($v) => null !== $v) === $this->dataOrigin) {
             return false;
         }
 

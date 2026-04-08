5e66c42ff625b92fe1ad28fbfae3b191f5bfb969
David mattei <d.mattei89@gmail.com>
2026-04-08 15:59:01 +0200

    fix(common/search): correct serialization suggest

--- EMS/common-bundle/src/Service/ElasticaService.php
+++ EMS/common-bundle/src/Service/ElasticaService.php
@@ -597,9 +597,8 @@ class ElasticaService
             $query->addAggregation($aggregation);
         }
 
-        $suggest = $search->getSuggest();
-        if (null !== $suggest && \count($suggest) > 0) {
-            $query->setSuggest($suggest);
+        if ([] !== $search->getSuggest()) {
+            $query->setParam('suggest', $search->getSuggest());
         }
 
         $esSearch = new ElasticaSearch($this->client);

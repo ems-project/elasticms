5b5adee746cd1bb2b0b1239fd8191eaec32ae4ba
David mattei <d.mattei89@gmail.com>
2026-04-15 14:35:45 +0200

    fix: linting

--- elasticms-web/config/reference.php
+++ elasticms-web/config/reference.php
@@ -1600,7 +1600,7 @@ use Symfony\Component\Config\Loader\ParamConfigurator as Param;
  *             use_underscore?: bool|Param, // Default: true
  *             unordered_list_markers?: list<scalar|Param|null>,
  *         },
- *         ...<mixed>
+ *         ...<string, mixed>
  *     },
  * }
  * @psalm-type ConfigType = array{

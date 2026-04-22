5b87fa78c8c934fdd733404b0160fcc0f80215a1
Mathieu De Keyzer <me@theus.be>
2026-04-22 12:16:55 +0200

    feat: abstract taht is abstract

--- EMS/core-bundle/src/Command/AbstractCoreCommand.php
+++ EMS/core-bundle/src/Command/AbstractCoreCommand.php
@@ -9,7 +9,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class AbstractCoreCommand extends AbstractCommand
+abstract class AbstractCoreCommand extends AbstractCommand
 {
     public const string OPTION_USERNAME = 'username';
     private string $username;

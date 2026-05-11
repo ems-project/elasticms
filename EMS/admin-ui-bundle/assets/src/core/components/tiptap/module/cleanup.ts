9a0ab7c09cd0c7c81fae5b36ab021596b78eefbc
David mattei <d.mattei89@gmail.com>
2026-05-11 13:24:09 +0200

    fix: removeFormat skip anchor & link

--- EMS/admin-ui-bundle/assets/src/core/components/tiptap/module/cleanup.ts
+++ EMS/admin-ui-bundle/assets/src/core/components/tiptap/module/cleanup.ts
@@ -1,6 +1,8 @@
 import IconClear from '@tabler/icons/outline/eraser.svg?raw'
 import { TiptapModule } from '../types.ts'
 
+const PRESERVED_MARKS = ['anchor', 'link']
+
 export const cleanupModule: TiptapModule = {
     toolbarGroup: 'cleanup',
     toolbar: [
@@ -11,7 +13,7 @@ export const cleanupModule: TiptapModule = {
             command: (e) => {
                 const chain = e.tiptap.chain().focus()
                 Object.keys(e.tiptap.schema.marks).forEach((mark) => {
-                    if (mark !== 'anchor') chain.unsetMark(mark)
+                    if (!PRESERVED_MARKS.includes(mark)) chain.unsetMark(mark)
                 })
                 chain.clearNodes().run()
             },
@@ -23,7 +25,8 @@ export const cleanupModule: TiptapModule = {
                 let hasFormatting = false
 
                 state.doc.nodesBetween(from, to, (node) => {
-                    if (node.marks.length > 0) hasFormatting = true
+                    if (node.marks.some((m) => !PRESERVED_MARKS.includes(m.type.name)))
+                        hasFormatting = true
                     if (!node.isText && node.type !== state.schema.nodes['paragraph'])
                         hasFormatting = true
                 })

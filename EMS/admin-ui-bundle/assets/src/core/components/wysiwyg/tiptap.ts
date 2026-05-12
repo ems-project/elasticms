77aa10b6fa3e786191565576b02dac84fcb46b70
David mattei <d.mattei89@gmail.com>
2026-05-12 16:01:06 +0200

    fix: toggle source code not disabling correctly

--- EMS/admin-ui-bundle/assets/src/core/components/wysiwyg/tiptap.ts
+++ EMS/admin-ui-bundle/assets/src/core/components/wysiwyg/tiptap.ts
@@ -118,7 +118,6 @@ export default class Tiptap {
                             tiptapEditor.setContent(this.textarea.value)
                             tiptapEditor.toolbar.setDisabled(false, ['Source', 'Maximize'])
                         }
-                        tiptapEditor.toolbar.update()
                     }
                 }
             ]

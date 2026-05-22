7e0618ae4752d04ae27d1184d684a2f91a127cfe
David mattei <d.mattei89@gmail.com>
2026-05-22 08:12:43 +0200

    feat(common/twig): add new ems_url_decode filter

--- EMS/common-bundle/src/Twig/TextRuntime.php
+++ EMS/common-bundle/src/Twig/TextRuntime.php
@@ -99,4 +99,20 @@ class TextRuntime implements RuntimeExtensionInterface
 
         return 0 === \count($violations);
     }
+
+    public function urlDecode(string $string): string|array
+    {
+        if (false === \mb_strpos($string, '=')) {
+            return \urldecode($string);
+        }
+
+        $params = [];
+
+        foreach (\explode('&', $string) as $chunk) {
+            $param = \explode('=', $chunk, 2);
+            $params[\urldecode($param[0])] = isset($param[1]) ? \urldecode($param[1]) : '';
+        }
+
+        return $params;
+    }
 }

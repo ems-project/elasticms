52bc117919b66a00a80da02032b4d1b3f77ac909
Davidmattei <d.mattei89@gmail.com>
2026-05-30 15:37:08 +0200

    fix: remove crypto-browserify
    We do not use this package and can be removed

--- EMS/form-bundle/webpack.config.js
+++ EMS/form-bundle/webpack.config.js
@@ -1,34 +1,36 @@
-const path = require('path');
-const webpack = require('webpack');
-const { WebpackManifestPlugin } = require('webpack-manifest-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
-const ESLintPlugin = require("eslint-webpack-plugin");
+const path = require('path')
+const webpack = require('webpack')
+const { WebpackManifestPlugin } = require('webpack-manifest-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
+const ESLintPlugin = require('eslint-webpack-plugin')
 
 module.exports = {
     mode: 'production',
     entry: {
-        'form': './assets/js/form.js',
-        'formDebug': './assets/js/formDebug.js',
-        'debug': './assets/js/debug.js',
-        'backend': './assets/js/backend.js',
-        'dynamicFields': './assets/js/dynamicFields.js',
-        'validation': './assets/js/validation.js'
+        form: './assets/js/form.js',
+        formDebug: './assets/js/formDebug.js',
+        debug: './assets/js/debug.js',
+        backend: './assets/js/backend.js',
+        dynamicFields: './assets/js/dynamicFields.js',
+        validation: './assets/js/validation.js'
     },
     optimization: {
-        minimizer: [new TerserPlugin({
-            extractComments: false,
-        })],
+        minimizer: [
+            new TerserPlugin({
+                extractComments: false
+            })
+        ]
     },
     plugins: [
         new ESLintPlugin({
-          extensions: ["js", "ts"],
-          emitWarning: true, // optionnel
+            extensions: ['js', 'ts'],
+            emitWarning: true // optionnel
         }),
-        new WebpackManifestPlugin({'publicPath': 'bundles/emsform/'}),
+        new WebpackManifestPlugin({ publicPath: 'bundles/emsform/' }),
         new CleanWebpackPlugin({
-            cleanOnceBeforeBuildPatterns: ['**/*', '!static/**'],
-        }),
+            cleanOnceBeforeBuildPatterns: ['**/*', '!static/**']
+        })
     ],
     output: {
         filename: 'js/[name].js',
@@ -36,17 +38,17 @@ module.exports = {
     },
     resolve: {
         fallback: {
-            "crypto": require.resolve("crypto-browserify"),
-            "buffer": require.resolve("buffer"),
-            "stream": require.resolve("stream-browserify"),
-            "vm": require.resolve("vm-browserify")
+            crypto: false,
+            buffer: require.resolve('buffer'),
+            stream: require.resolve('stream-browserify'),
+            vm: require.resolve('vm-browserify')
         }
     },
     module: {
         rules: [
             {
                 test: /\.m?js$/,
-                resolve: { fullySpecified: false },
+                resolve: { fullySpecified: false }
             },
             {
                 test: /\.css$/,
@@ -54,10 +56,8 @@ module.exports = {
             },
             {
                 test: /\.(png|jpg|gif)$/i,
-                use: [
-                    { loader: 'url-loader', options: { limit: 10000, name: 'img/[name].[ext]' } }
-                ]
+                use: [{ loader: 'url-loader', options: { limit: 10000, name: 'img/[name].[ext]' } }]
             }
-        ],
+        ]
     }
-};
+}

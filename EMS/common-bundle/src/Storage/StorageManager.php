a0943486d2e96462208e3ead1daa1d409dddf9d7
Mathieu De Keyzer <me@theus.be>
2026-08-26 10:48:17 +0200

    fix: variable mispatched

--- EMS/common-bundle/src/Storage/StorageManager.php
+++ EMS/common-bundle/src/Storage/StorageManager.php
@@ -831,6 +831,6 @@ class StorageManager implements FileManagerInterface
 
     private function getNotAvailableStorageCacheItem(StorageInterface $storage): CacheItemInterface
     {
-        return $this->cacheManager->getItem($this->computeDataHash([\sprintf('%s is not available', $storage->__toString())]));
+        return $this->cacheManager->getItem(\sprintf('storage_service_na_%s', \hash($this->hashAlgo, $storage->__toString())));
     }
 }

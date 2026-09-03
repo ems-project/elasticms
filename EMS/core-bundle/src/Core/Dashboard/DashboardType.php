29b22213cbc8b86c21a5ca5dd7127490acab1f4b
David mattei <d.mattei89@gmail.com>
2026-09-03 10:35:13 +0200

    fix: improve translation dashboard type

--- EMS/core-bundle/src/Core/Dashboard/DashboardType.php
+++ EMS/core-bundle/src/Core/Dashboard/DashboardType.php
@@ -4,22 +4,23 @@ declare(strict_types=1);
 
 namespace EMS\CoreBundle\Core\Dashboard;
 
-use Symfony\Component\Translation\TranslatableMessage;
+use Symfony\Contracts\Translation\TranslatableInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 use function Symfony\Component\Translation\t;
 
-enum DashboardType: string
+enum DashboardType: string implements TranslatableInterface
 {
     case EXPORT = 'ems_core.dashboard.export';
     case REVISION_TASK = 'ems_core.dashboard.revision_task';
     case TEMPLATE = 'ems_core.dashboard.template';
 
-    public function getLabel(): TranslatableMessage
+    public function trans(TranslatorInterface $translator, ?string $locale = null): string
     {
         return match ($this) {
-            self::EXPORT => t('ems_core.dashboard.export.label', [], 'emsco-core') ,
-            self::REVISION_TASK => t('ems_core.dashboard.revision_task.label', [], 'emsco-core'),
-            self::TEMPLATE => t('ems_core.dashboard.template.label', [], 'emsco-core')
+            self::EXPORT => t('ems_core.dashboard.export.label', [], 'emsco-core')->trans($translator),
+            self::REVISION_TASK => t('ems_core.dashboard.revision_task.label', [], 'emsco-core')->trans($translator),
+            self::TEMPLATE => t('ems_core.dashboard.template.label', [], 'emsco-core')->trans($translator)
         };
     }
 }

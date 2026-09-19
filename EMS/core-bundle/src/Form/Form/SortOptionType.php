4c767d018b827fdaa886546e64ef4f4081665c45
Mathieu De Keyzer <me@theus.be>
2026-09-19 13:01:19 +0200

    chore: phpcs

--- EMS/core-bundle/src/Form/Form/SortOptionType.php
+++ EMS/core-bundle/src/Form/Form/SortOptionType.php
@@ -10,6 +10,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+
 use function Symfony\Component\Translation\t;
 
 /**

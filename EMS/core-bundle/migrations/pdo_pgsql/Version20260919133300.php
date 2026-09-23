ede72b84d8ba444d215c445e66025763a480aedb
Mathieu De Keyzer <me@theus.be>
2026-09-23 09:43:02 +0200

    fix: typo

--- EMS/core-bundle/migrations/pdo_pgsql/Version20260919133300.php
+++ EMS/core-bundle/migrations/pdo_pgsql/Version20260919133300.php
@@ -109,7 +109,7 @@ final class Version20260919133300 extends AbstractMigration
         $defaultFilters = [[
             'booleanClause' => 'must',
             'field' => '',
-            'operator' => 'must_et',
+            'operator' => 'must_and',
             'pattern' => '',
             'boost' => '',
             'minimum_should_match' => 1,

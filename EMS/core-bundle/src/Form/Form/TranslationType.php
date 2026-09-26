fde54cc2eb9f65eac43d9657cdc5b3f5f2822915
Mathieu De Keyzer <me@theus.be>
2026-09-26 13:33:38 +0200

    feat: TranslationsType

---
+++ EMS/core-bundle/src/Form/Form/TranslationType.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EMS\CoreBundle\Form\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Intl\Locales;
+
+use function Symfony\Component\Translation\t;
+
+/**
+ * @extends AbstractType<mixed>
+ */
+final class TranslationType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface<mixed> $builder
+     * @param array<string, mixed>        $options
+     */
+    #[\Override]
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('locale', ChoiceType::class, [
+                'label' => t('field.locale', [], 'emsco-core'),
+                'required' => true,
+                'choices' => \array_flip(Locales::getNames()),
+                'choice_translation_domain' => false,
+            ])
+            ->add('label', TextType::class, [
+                'label' => t('field.label', [], 'emsco-core'),
+                'required' => true,
+            ]);
+    }
+}
---
+++ EMS/core-bundle/src/Form/Form/TranslationsType.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EMS\CoreBundle\Form\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+use function Symfony\Component\Translation\t;
+
+/**
+ * @extends AbstractType<mixed>
+ */
+final class TranslationsType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface<mixed> $builder
+     * @param array<string, mixed>        $options
+     */
+    #[\Override]
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+    }
+
+    #[\Override]
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'allow_add' => true,
+            'allow_delete' => true,
+            'entry_options' => [
+                'label' => false,
+            ],
+            'entry_type' => TranslationType::class,
+            'label' => t('field.translations', [], 'emsco-core'),
+        ]);
+    }
+
+    #[\Override]
+    public function getParent(): string
+    {
+        return CollectionType::class;
+    }
+}
